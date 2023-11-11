ae70f9b94a08a44930e401ae2cd357955c33bd2d
Delowar Hossan <delowar.designer@gmail.com>
2023-11-11 07:08:16 -0800

    python Programming user input in python

---
+++ Python Programming Crash Course Delowar/23-user-input.py
@@ -0,0 +1,8 @@
+# ------- user input in python -------
+#name = input("Enter your Name: ")
+#print("Your Name ",name)
+
+num1 = input("Enter Number one: ")
+num2 = input("Enter Number Second: ")
+result = int(num1)+int(num2)
+print(result)
