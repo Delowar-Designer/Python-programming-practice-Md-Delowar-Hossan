a4094c9cf811db9314c1dd8ca82257d96bf06f61
Delowar Hossan <delowar.designer@gmail.com>
2023-11-13 01:39:20 -0800

    python Lists solving

--- Python Programming Crash Course Delowar/06-lists.py
+++ Python Programming Crash Course Delowar/06-lists.py
@@ -1,1 +1,48 @@
 # ------ Lists in python-----
+# listt in a ordered xontainer
+# create a list
+#pets = []#empty list
+#Pets = ["Dog","Cat","Rabbit"]
+            # positive Element  0, 1, 2
+            # Negative Element -3, -2, -1
+#print(Pets) 
+#-- Indexing
+#print(Pets[0])#access the 1st element
+#print(Pets[2])#access the Last element
+#-- Negative indexing
+#print(Pets[-3]) 
+
+#-- Ronge of indexes
+#print(Pets[1:3])
+
+#-- Adding Items to a list
+#Pets.append("Elephent")
+#print(Pets)
+
+#Pets.insert(0,"Delowar")
+#print(Pets)
+
+#-- Deleting items from a list
+#Pets.pop()
+#print(Pets)
+
+#Pets.remove("Cat")
+#print(Pets)
+#-- Getting the length of a list
+#print(len(Pets))
+
+# --Changing an item value
+#Pets[1] = "Fish"
+#print(Pets)
+
+# ---checr if on item exists 
+# membership operator - in ont in
+Frent = ["Delowar",2,5.2,"Majudur","Joty","Mito"]
+check_My = "Majudur" not in Frent
+print(check_My)
+
+#-- Extending a list
+#num1 = [1, 2, 3]
+#num2 = [4, 5, 6]
+#num1.extend(num2)
+#print(num1)
