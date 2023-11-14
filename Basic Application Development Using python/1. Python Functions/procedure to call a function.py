b9af328c051503658c04e6cb1e7bb3f8b6a3cbf3
Delowar Hossan <delowar.designer@gmail.com>
2023-11-14 02:58:59 -0800

    procedure to call a function

---
+++ Basic Application Development Using python/1. Python Functions/procedure to call a function.py
@@ -0,0 +1,20 @@
+#---procedure to call a function
+
+'''
+def FunctionName(parameters):
+    Statements
+    return [expression]
+'''
+
+
+def mul(x,y):
+    return(x*y)
+print(mul(10,15))
+
+#   অথবা
+def div(x, y):
+    z = x / y
+    print(z)
+div(10, 5)
+
+
