285ff04729342239f9568c4e9d4ce48f2b57246c
Delowar Hossan <delowar.designer@gmail.com>
2023-11-14 02:42:03 -0800

    Calling a function repeatedly

---
+++ Basic Application Development Using python/1. Python Functions/Calling a function repeatedly.py
@@ -0,0 +1,12 @@
+#--Calling a function repeatedly--
+def add_fun(a,b):
+    c=a+b
+    return c
+print("Addition A & B =",add_fun(15,30))
+print("Addition A & B =",add_fun(15,16))
+print("Addition A & B =",add_fun(25,26))
+
+'''
+স্টান্ডার্ড লাইব্রেরি ফাংশন বা বিল্ট ইন ফাংশন:
+input(), int(), float(), sqrt(), sum(), max(), min(), len(), sorted(), count() etc.
+'''
