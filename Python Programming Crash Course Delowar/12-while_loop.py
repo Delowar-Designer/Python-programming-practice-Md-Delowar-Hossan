b67bdb260b77bdcceb25a4a56bb60ac3989dcd34
Delowar Hossan <delowar.designer@gmail.com>
2023-11-14 01:52:21 -0800

    while loop in python

---
+++ Python Programming Crash Course Delowar/12-while_loop.py
@@ -0,0 +1,12 @@
+#----- while loop in python---
+'''
+    while condition:
+        statements
+'''
+#creating hellow world 5 timees
+i = 1 #loop iniatilization
+while i<=5: #loop condition
+    #while loop body
+    print("Hello Delowar Programmer")
+    i += 1 #increament part
+
