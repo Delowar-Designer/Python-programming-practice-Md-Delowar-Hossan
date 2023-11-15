8c8c8ebea1e2f3adec09f3da09d6f1c55e2a0a12
Delowar Hossan <delowar.designer@gmail.com>
2023-11-15 03:27:59 -0800

    Math modules

---
+++ Python Programming Crash Course Delowar/18-modules And Pip.py
@@ -0,0 +1,52 @@
+# modules Math and date and time
+
+
+# math modules sqrt
+import math
+number = 2 
+result = math.sqrt(number)
+print("Sqrt:",result)
+
+# math modules pow
+import math
+number1 = 2 
+number2 = 3
+result = math.pow(number1, number2)
+print("Pow:",result)
+
+# math modules ceil
+import math
+number3 = 2.0 
+result = math.ceil(number3)
+print("Ceil:",result)
+
+# math modules pi
+pi = math.pi
+print("Pi:",pi)
+
+# math modules cos
+import math
+number4 = 2
+result = math.cos(number4)
+print("Cos:",result)
+
+# math modules sin
+import math
+number5 = 2
+result = math.sin(number5)
+print("Sin:",result)
+
+# math modules tan
+import math
+number6 = 2
+result = math.tan(number6)
+print("Tan:",result)
+
+
+# math modules cosh
+import math
+number7 = 2
+result = math.cosh(number7)
+print("Cosh:",result)
+
+
