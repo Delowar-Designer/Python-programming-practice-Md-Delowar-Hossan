656a75dc419e4e0ba38d8ee945d17af29c8e20aa
Delowar Hossan <delowar.designer@gmail.com>
2023-11-15 00:52:50 -0800

    Break and Continue is python

---
+++ Python Programming Crash Course Delowar/15-Break and Continue.py
@@ -0,0 +1,10 @@
+# --- Break and continue is python ---
+
+animal = ["tiger","cat","dog"]
+for pet in animal:
+    if pet == "cat":
+        #break
+        continue
+    #print("Break: ",pet)
+    print("Continue: ",pet)
+    
