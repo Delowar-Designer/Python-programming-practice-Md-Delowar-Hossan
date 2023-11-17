200dd7084ff6880c62e82a41a4c6641c516da9ea
Delowar Hossan <delowar.designer@gmail.com>
2023-11-17 02:16:05 -0800

    Vraiabele and Operators in Python

---
+++ Python Programming Crash Course Delowar/vraiabele.py
@@ -0,0 +1,109 @@
+myname = "Delowar"
+myname = "Hossan"
+myage = 18
+myage = 17
+print(myname)
+print(myage)
+
+'''
+# wright way
+myconutry
+my_country
+_mycountry
+mycountry1
+myCoountry
+
+# Wrong Way
+$@mycountry
+my country
+my-country
+120mycountry
+'''
+# Datatype
+a = 5
+b = 2.25
+c = 2+3j
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+string1 = 'this is string'
+strint2 = "this is also string"
+string3 = '''My name is "Delowar Hossan"
+my age is "18" years
+my favourite subject is cse'''
+print(string1)
+print(strint2)
+print(string3)
+
+fname = "Delowar"
+lname = "Hossan"
+print("My name is ",fname,"and my surname is ",lname)
+
+
+a = True
+b = False
+
+print(type(a))
+print(type(b))
+
+
+# Operators in python Arithmetic 
+a =  10
+b = 5
+sum = a+b
+sub = a-b
+mul = a*b
+div1 = a/b
+div2 = a//b
+mod = a%b
+p = a**b
+print("The addition of a and b is: ", sum)
+print("The substraction of a and b is: ", sub)
+print("The multipilication of a and b is: ", mul)
+print("The division of a and b is: ", div1)
+print("The division of a and b is: ", div2)
+print("The modulus of a and b is: ", mod)
+print("The power of a and b is: ", p)
+
+# Operators in python Assignment ==, +=, -=, *=, %=, **=, //= 
+x = 10
+x += 5 # x+=5 a=a+5 a=10+5=15
+print(x)
+
+# Operators in python Comparison ==, !=, <=, >=, <, >
+c = 15
+y = 22
+
+print(c>y)
+print(c>=y)
+print(c<y)
+print(c<=y)
+print(c==y)
+print(c!=y)
+
+# Operators in python Logical and, or, not
+E=5
+F=7
+logical_and = E<F and E>F
+logical_or = E>F or E<F
+logical_not = (not(E<F and E>F))
+print("Logitcal and Operators:",logical_and)
+print("Logitcal or Operators:",logical_or)
+print("Logitcal not Operators:",logical_not)
+
+
+# Operators is python Membership in, Not in
+txt = "I Love Python Programming"
+print("Love" in txt)
+print("Python" not in txt)
+
+# Python Identity operators is, is not
+N = 5
+M = 5
+print(N is M)
+print(N is not M)
+
+
+
