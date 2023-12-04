77c04156cc2c8176b7d8d54d66528c9bb6fc8316
Delowar-Designer <delowar.designer@gmail.com>
2023-12-04 10:01:04 -0800

    Use of function Return and None return

---
+++ Basic Application Development Using python/1. Python Functions/Use of function Return and None return.py
@@ -0,0 +1,11 @@
+#Use of function Return and None return
+def my_func1():
+    print("Hello world")
+    return None
+
+def my_func2():
+    print("Helow World")
+    return
+
+def my_func3():
+    print("Helow world")
