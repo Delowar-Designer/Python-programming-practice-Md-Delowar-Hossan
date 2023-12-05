56713f7d8d06a287216277ace72b709248215c7f
Delowar-Designer <delowar.designer@gmail.com>
2023-12-04 21:41:44 -0800

    The Function current Date

---
+++ Basic Application Development Using python/1. Python Functions/The Function Current time.py
@@ -0,0 +1,13 @@
+#The Function Current time
+import time
+localtime=time.localtime(time.time())
+print("local current time is:",localtime)
+
+
+# The Function current Date
+from datetime import time
+from datetime import date
+from datetime import datetime
+today=date.today()
+print("Current Date is :",today)
+
