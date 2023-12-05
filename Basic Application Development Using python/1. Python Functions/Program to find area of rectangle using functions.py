5a208b2a08889ac4afd713926039813adb56450a
Delowar-Designer <delowar.designer@gmail.com>
2023-12-04 23:31:27 -0800

    Program to find area of rectangle using functions

---
+++ Basic Application Development Using python/1. Python Functions/Program to find area of rectangle using functions.py
@@ -0,0 +1,7 @@
+#Program to find area of rectangle using functions.
+def Area():
+    length=int(input("Enther the value of Length:"))
+    width=int(input("Enther the value of Width:"))
+    RectangleArea=length*width
+    print("Area of Rectanglae is=",RectangleArea)
+Area()
