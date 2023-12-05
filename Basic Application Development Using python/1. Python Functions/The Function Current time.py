8d0b8f5f7ce585cbc8eb17da7174d659d92ff9c4
Delowar-Designer <delowar.designer@gmail.com>
2023-12-04 22:02:41 -0800

    Find today's date and time

--- Basic Application Development Using python/1. Python Functions/The Function Current time.py
+++ Basic Application Development Using python/1. Python Functions/The Function Current time.py
@@ -11,3 +11,29 @@ from datetime import datetime
 today=date.today()
 print("Current Date is :",today)
 
+
+#The Function Yesterday date
+from datetime import date
+from datetime import timedelta
+def Yesterday():
+    today=date.today()
+    Yesterday=today-timedelta(days=1)
+    return Yesterday
+print("Todays Date is :", date.today())
+print("Yesterday was :",Yesterday())
+
+
+#The Function Tomorrow date
+from datetime import date
+from datetime import timedelta
+def tomorrow():
+    return date.today()+timedelta(days=1)
+print("Todays Date is :",date.today())
+print("Tomorrow Date Will be :",tomorrow())
+
+# The Function Find today's date and time
+from datetime import time
+from datetime import date
+from datetime import datetime
+today=datetime.today()
+print("Todays Date & Time is :",today)
