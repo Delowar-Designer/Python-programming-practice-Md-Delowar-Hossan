dc87ad65d232d58a3c7d790c6746ec035e02411d
Delowar-Designer <delowar.designer@gmail.com>
2023-12-13 21:12:47 -0800

    list problem

---
+++ Python Programming Crash Course Delowar/list_problem1.py
@@ -0,0 +1,20 @@
+f1 = input("Enter your favourite on 1:")
+f2 = input("Enter your favourite on 2:")
+f3 = input("Enter your favourite on 3:")
+f4 = input("Enter your favourite on 4:")
+f5 = input("Enter your favourite on 5:")
+
+my_favourite_food = [f1,f2,f3,f4,f5]
+print(my_favourite_food)
+
+#sort mathod
+s1 = int(input("Enter your 1st on: "))
+s2 = int(input("Enter your 2st on: "))
+s3 = int(input("Enter your 3st on: "))
+s4 = int(input("Enter your 4st on: "))
+s5 = int(input("Enter your 5st on: "))
+
+user_on = [s1,s2,s3,s4,s5]
+user_on.sort()
+print(user_on)
+
---
+++ Python Programming Crash Course Delowar/list_problem2.py
@@ -0,0 +1,9 @@
+my_tuple = (5,10,15,20,25)
+print(my_tuple)
+my_tuple = 20
+print(my_tuple)
+
+
+my_list = ["python",12,12,5,Trus,"js"]
+print("python" in my_list)
+print(52 in my_list)
