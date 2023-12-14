8d0eaeb16d11afa54670444f7d2dd9523441a0d3
Delowar-Designer <delowar.designer@gmail.com>
2023-12-13 21:19:00 -0800

    list problem2

--- Python Programming Crash Course Delowar/list_problem2.py
+++ Python Programming Crash Course Delowar/list_problem2.py
@@ -4,6 +4,10 @@ my_tuple = 20
 print(my_tuple)
 
 
-my_list = ["python",12,12,5,Trus,"js"]
+my_list = ["python", 12, 12, 5, True, "js"]
 print("python" in my_list)
 print(52 in my_list)
+
+x = [5,10,15,20]
+x.reverse()
+print(x)
