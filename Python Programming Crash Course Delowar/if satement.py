a169ff002b93579c3aa06c95a05496d87becf9f6
Delowar-Designer <delowar.designer@gmail.com>
2023-12-14 02:15:25 -0800

    if statement

---
+++ Python Programming Crash Course Delowar/if satement.py
@@ -0,0 +1,12 @@
+# if statement
+num = int(input("enter a no: "))
+if num>=0:
+    print("positive number")
+elif num==0:
+    #body of elif
+    print("zero")
+else:
+    #body of else
+    print("negative no")
+
+
