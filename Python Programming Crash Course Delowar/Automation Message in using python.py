32c9541a43d0fb19f285d32aa3dd0af013912288
Delowar-Designer <delowar.designer@gmail.com>
2023-12-15 15:17:40 +0600

    Automation Message in using Python

---
+++ Python Programming Crash Course Delowar/Automation Message in using python.py
@@ -0,0 +1,11 @@
+#Automation Message in using python
+"""import pywhatkit as pw 
+pw.sendwhatmsg('+8801773873289','Aslamualai Hello there 👋',12,42,12)"""
+
+import pyautogui
+import time
+
+while True:
+    time.sleep(3)
+    pyautogui.typewrite("Please subscripe")
+    pyautogui.press('Enter')
