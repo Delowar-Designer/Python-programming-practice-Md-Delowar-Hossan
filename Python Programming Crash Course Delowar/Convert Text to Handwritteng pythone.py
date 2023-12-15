0a9cc50e6866b335b154bde2f6ab7b8c51483ea5
Delowar-Designer <delowar.designer@gmail.com>
2023-12-15 16:11:08 +0600

    text

--- Python Programming Crash Course Delowar/Convert Text to Handwritteng pythone.py
+++ Python Programming Crash Course Delowar/Convert Text to Handwritteng pythone.py
@@ -1,9 +1,18 @@
 # Convart Text to Handwritteng pythone
+#import pywhatkit as pw
+
+#text = """A paragraph develops ONE main idea through a series of related sentences. 
+#This main idea is usually introduced in the first sentence of the paragraph, 
+#called the topic sentence. The idea is then developed further through the sentences that follow."""
+
+#pw.text_to_handwriting(text, 'demo1.png', [0, 0, 1, 138])
+#print("END")
+
 import pywhatkit as pw
 
-txt = """A paragraph develops ONE main idea through a series of related sentences. 
-This main idea is usually introduced in the first sentence of the paragraph, 
-called the topic sentence. The idea is then developed further through the sentences that follow."""
+text = """A paragraph develops ONE main idea through a series of related sentences. 
+This main idea is usually introduced in the first sentence of the paragraph"""
 
-pw.text_to_handwriting(txt, 'demo1.png', [0, 0, 1, 138])
+# Convert text to handwriting
+pw.text_to_handwriting(text, '1111.png', rgb=[0, 0, 0, 255])
 print("END")
