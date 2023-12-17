0cd44afdc12a2126aa8401340d4258c3360bf85f
Delowar-Designer <delowar.designer@gmail.com>
2023-12-17 21:10:06 +0600

    Variables and Data Types

---
+++ Python Bangla Tutorials/program5.py
@@ -0,0 +1,11 @@
+#Variables and Data Types
+name = "Delowar Hossan"
+age = 19
+cgpa = 4.82
+print("Out new Student name is "+name)
+print(name+" lives in Dinajpur")
+print("He is currently ",age," years old")
+print("At the age of ",age,", has startend to learn python")
+print(name +" has scored ",cgpa," in her Secondary exam")
+
+print("Md"+" Delowar "+"Hossan")
