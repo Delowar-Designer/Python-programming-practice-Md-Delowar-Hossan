1b7998bab078c5c3e6eff3e3bf62f83985d3a838
Delowar-Designer <delowar.designer@gmail.com>
2023-12-18 00:05:58 +0600

    Badic Numerical Operations

---
+++ Python Bangla Tutorials/1. Badic Numerical Operations.py
@@ -0,0 +1,17 @@
+# Badic Numerical Operations
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10%5)
+a=27
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+
