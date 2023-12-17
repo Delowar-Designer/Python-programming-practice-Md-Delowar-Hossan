b87d30b858d5d63a723429bb4bf12d378d1c176b
Delowar-Designer <delowar.designer@gmail.com>
2023-12-17 12:56:57 +0600

    Escape Sequences

--- Python Bangla Tutorials/Program2.py
+++ Python Bangla Tutorials/Program2.py
@@ -8,3 +8,7 @@ comment'''
 
 print("Hello World")
 print("Delowar Hossan")
+print("Hello Delowar Hossan \nMy company Delowar.Designer")
+print("Hello Delowar Hossan \tMy company Delowar.Designer")
+print("\"Delowar Hossan\"")
+print("\'Delowar Hossan\'")
