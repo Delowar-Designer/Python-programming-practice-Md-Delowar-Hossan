4f073b4c07c882b5dd812366a3c4deaa38f9a841
Delowar-Designer <delowar.designer@gmail.com>
2023-12-18 21:46:23 +0600

    Type casting

---
+++ Python Bangla Tutorials/3. Type casting.py
@@ -0,0 +1,44 @@
+# Type casting
+
+num1 = input("Enter first Number : ")
+num2 = input("Enter second Number : ")
+
+sum = int(num1) + int(num2)
+print("The sum of", sum)
+
+numbers1 = int(input("Enter first Number : "))
+numbers2 = int(input("Enter second Number : "))
+
+Result = numbers1 + numbers2
+print("The result is + ", Result)
+
+Result = numbers1 - numbers2
+print("The result is - ", Result)
+
+Result = numbers1 * numbers2
+print("The result is * ", Result)
+
+Result = numbers1 / numbers2
+print("The result is / ", Result)
+
+Result = numbers1 % numbers2
+print("The result is % ", Result)
+
+Result = numbers1 // numbers2
+print("The result is // ", Result)
+
+Result = numbers1 < numbers2
+print("The result is < ", Result)
+
+Result = numbers1 > numbers2
+print("The result is > ", Result)
+
+Result = numbers1 & numbers2
+print("The result is & ", Result)
+
+Result = numbers1 ^ numbers2
+print("The result is ^ ", Result)
+
+Result = numbers1 ** numbers2
+print("The result is ** ", Result)
+
