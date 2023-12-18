095f75691fabafc53373a48b58295db67df51970
Delowar-Designer <delowar.designer@gmail.com>
2023-12-18 21:15:35 +0600

    Getting user input

---
+++ Python Bangla Tutorials/2. Getting User Input.py
@@ -0,0 +1,11 @@
+# Getting user input
+
+name = input("Enter your name: ")
+age = input("Enter your ate : ")
+gpa = input("Enter your gpa : ")
+
+print("Student Inofrmation")
+print("---------------------")
+print("Your Name : "+name)
+print("Your Age : "+age)
+print("Your GPA : "+gpa)
