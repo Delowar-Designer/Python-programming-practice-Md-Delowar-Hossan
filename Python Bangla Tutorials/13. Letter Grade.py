f749ec612b90f75a371c083ed3d9811d25f9757d
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 11:49:15 +0600

    Letter Grade

---
+++ Python Bangla Tutorials/13. Letter Grade.py
@@ -0,0 +1,13 @@
+# Letter Grade
+marks = 79
+if marks >= 80 and marks <= 100 :
+    print("A+")
+elif marks >= 70 and marks <= 79 :
+    print("A")
+
+
+marks2 = 70
+if 80 <= marks2 <= 100 :
+    print("mark2 is A+")
+if 70 <= marks2 <= 79 :
+    print("mark2 is A")
