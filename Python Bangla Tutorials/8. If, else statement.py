bc40a54539303778900f87dae3b18e1efbfe8e78
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 10:24:03 +0600

    if else Statement

---
+++ Python Bangla Tutorials/8. If, else statement.py
@@ -0,0 +1,34 @@
+# If, else statement
+# pass/fail
+
+mark = 20
+if mark >= 33:
+    print("pass")
+if mark < 33:
+    print("Fail")
+
+mark2 = 48
+if mark2 >= 33:
+    print("pass")
+else:
+    print("Fail")
+
+# to number Largest
+num1 = 20
+num2 = 80
+
+if num1 > num2:
+    print(num1)
+
+else:
+    print(num2)
+
+# even/add
+num3 = 4
+if num3 % 2 == 0:
+    print("Even")
+    print(num3)
+else:
+    print("odd")
+    print(num3)
+
