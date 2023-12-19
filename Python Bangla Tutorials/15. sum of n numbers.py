dc7b15c237ed00f68264fa1268cf0059624dc8b8
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 12:33:56 +0600

    Sum of n Numbers

---
+++ Python Bangla Tutorials/15. sum of n numbers.py
@@ -0,0 +1,16 @@
+# sum of n numbers
+sum = 0
+i = 1
+while i <= 5:
+    sum = sum + i
+    i = i + 1
+print("sum of numbers",sum)
+
+# 2 + 4 ..... + n ?
+n = int(input("Enter a last number: "))
+sum1 = 0
+D = 1
+while D <= n :
+    sum1 = sum1 + D
+    D = D + 1
+print("sum of",sum1)
