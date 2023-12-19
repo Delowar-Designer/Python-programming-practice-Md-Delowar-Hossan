e6e2980533fe0dda3609f371de79b29d299fa538
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 09:14:21 +0600

    Library Functions of Math

---
+++ Python Bangla Tutorials/5. Library Functions of Math.py
@@ -0,0 +1,16 @@
+# Library Functions of Math
+from math import *
+
+result = max(20,10)
+print(result)
+print(type(result))
+
+print(max(12,2))
+print(min(12,2))
+print(abs(-4))
+print(pow(2, 3))
+print(sqrt(25))
+print(round(4.8))
+print(floor(3.4))
+print(ceil(3.7))
+
