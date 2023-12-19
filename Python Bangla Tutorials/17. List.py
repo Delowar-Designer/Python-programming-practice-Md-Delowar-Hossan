74db3cf83b207e882b47e782b714696ef4decdea
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 13:31:40 +0600

    List

---
+++ Python Bangla Tutorials/17. List.py
@@ -0,0 +1,12 @@
+# List
+subjects = ["c","c++","java","python","Android","os","Toc"]
+print(subjects)
+print(len(subjects))
+print(subjects[0])
+print(subjects[2:])
+print(subjects[-1])
+print("python" in subjects)
+print("swift" in subjects)
+print("Delowar" not in subjects)
+print(subjects + ["swift",27])
+print(subjects * 2)
