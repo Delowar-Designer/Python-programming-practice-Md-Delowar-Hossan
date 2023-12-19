e545132d6623af14c4190d9935f5ecf232be7c00
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 12:11:29 +0600

    Loop While Loop

---
+++ Python Bangla Tutorials/14. Loop - While Loop.py
@@ -0,0 +1,13 @@
+# Loop - While Loop
+i = 0
+while i <= 100:
+    print(i)
+    i = i + 1
+print("Goodbye")
+
+a = 1
+while a <= 5:
+    print(a,"I Love You")
+    a = a + 1
+print("Program Ended")
+
