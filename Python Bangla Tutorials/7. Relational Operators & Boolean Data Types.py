0e4c27cdf69151d0b9d5a7e36317f2da1f54ce9e
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 10:03:02 +0600

    Relational Operators & Boolean Data Types

---
+++ Python Bangla Tutorials/7. Relational Operators & Boolean Data Types.py
@@ -0,0 +1,11 @@
+# Relational Operators & Boolean Data Types
+result = 30 > 20
+print(result)
+
+print(30 > 20)
+print(30 < 20)
+print(30 >= 20)
+print(30 <= 20)
+print(30 == 20)
+print(30 != 20)
+print("Delowar"=="Delowar")
