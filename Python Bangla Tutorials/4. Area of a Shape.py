648968cd8f9f9c54334966adca850a64517df1da
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 08:49:35 +0600

    Area of a Shape

---
+++ Python Bangla Tutorials/4. Area of a Shape.py
@@ -0,0 +1,21 @@
+# Area of a Shape
+# What is the area of this triangle?
+
+base = float(input("Enter triangle Base = "))
+height = float(input("Enter triangle height = "))
+
+triangle_area = 0.5 * base * height
+print("Area of triangle ", triangle_area)
+
+# What is the area of this circle?
+radius = float(input("Enter circle of radius Number = "))
+circle_area = radius * radius * 3.14159
+print("Area of Circle ", circle_area)
+
+# What is the area of this rectangle?
+width = float(input("Enter rectangle of width"))
+height = float(input("Enter rectangle of height"))
+rectangle_area = width * height
+print("Area of Rectangle ", rectangle_area)
+
+
