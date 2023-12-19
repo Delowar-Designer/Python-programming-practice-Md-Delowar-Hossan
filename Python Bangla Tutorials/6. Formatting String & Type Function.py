061b4914c3b09202fb9fe5cdb0673e39cd9d1e30
Delowar-Designer <delowar.designer@gmail.com>
2023-12-19 09:37:46 +0600

    Formatting String & Type Function

---
+++ Python Bangla Tutorials/6. Formatting String & Type Function.py
@@ -0,0 +1,23 @@
+# Formatting String & Type Function
+num = 20
+print(type(num))
+
+num1 = 20.5
+print(type(num1))
+
+num2 = "Delowar"
+print(type(num2))
+
+print(type(True))
+
+num3 = 24
+num4 = 60
+print(num3+num4)
+print("Sum is", num3+num4)
+print(f"{num3} + {num4} = {num3+num4} ")
+
+print("My Name is")
+print("Mitu")
+print("My Name is ", end="")
+print("Md Delowar Hossan")
+
