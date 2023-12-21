40c5042dc84bbf97877208de8b8858b085ceb988
Delowar-Designer <delowar.designer@gmail.com>
2023-12-21 10:39:41 +0600

    Zip Function

---
+++ Python Bangla Tutorials/43.  Zip Function.py
@@ -0,0 +1,5 @@
+# zip Function
+roll = [101, 102, 103, 104, 105, 106, 107, 108]
+name = ["John", "Smith", "Mitu", "Joty", "Delowar", "Majdur", "Yafta", "Antor"]
+
+print(list(zip(roll, name,"ABCDEFAB")))
