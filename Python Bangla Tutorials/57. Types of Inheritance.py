6ec8ff416c81113e91efb53de96f4322314b9147
Delowar-Designer <delowar.designer@gmail.com>
2023-12-21 17:23:29 +0600

    Types of Inheritance

---
+++ Python Bangla Tutorials/57. Types of Inheritance.py
@@ -0,0 +1,41 @@
+# Types of Inheritance
+'''class A:
+    def display1(self):
+        print("I am inside A class")
+
+class B(A):
+    # display1()
+    def display2(self):
+        print("I am inside B class")
+
+class C(B):
+    # display1()
+    # display2()
+    def display3(self):
+        super().display1()
+        super().display2()
+        print("I am inside C class")
+
+
+ob1 = C()
+# ob1.display1()
+# ob1.display2()
+ob1.display3()'''
+
+
+class A:
+    def display(self):
+        print("I am inside A class")
+
+class B:
+    # display1()
+    def display(self):
+        print("I am inside B class")
+
+class C(B,A):
+    # A -> display()
+    # B -> display()
+   pass
+
+ob1 = C()
+ob1.display()
