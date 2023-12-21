d0a373f89828050545e2db39697c09424d634cb6
Delowar-Designer <delowar.designer@gmail.com>
2023-12-21 14:36:49 +0600

    Swapping

---
+++ Python Bangla Tutorials/49. Swapping.py
@@ -0,0 +1,15 @@
+# Swapping
+a = 20
+b = 10
+temp = a #temp =20
+a = b # a =10
+b = temp # b = 20
+
+print("a = ", a)
+print("b = ", b)
+
+a = 12
+b = 22
+a, b = b, a
+print("a = ", a)
+print("b = ", b)
