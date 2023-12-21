ff8aff5dd6a7a508c30941964f1fab8fdccd8791
Delowar-Designer <delowar.designer@gmail.com>
2023-12-21 07:10:18 +0600

    Set

---
+++ Python Bangla Tutorials/30. Set.py
@@ -0,0 +1,16 @@
+# Set
+num = {1,2,3,4,5,6,7,8,9}
+num1 = set([0,2,4,11,12,13,14,15,16,17,18])
+
+print(num)
+print(num1)
+num1.add(188)
+print(num1)
+num1.remove(0)
+print(num1)
+print(15 in num1)
+print(1 not in num1)
+print(num | num1)
+print(num & num1)
+print(num - num1)
+print(num ^ num1)
