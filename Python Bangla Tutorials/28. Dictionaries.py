4f7074369a35025bcb154f5eec1bc427646a7381
Delowar-Designer <delowar.designer@gmail.com>
2023-12-21 06:44:01 +0600

    Dictionaries

---
+++ Python Bangla Tutorials/28. Dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries
+studentid = {
+    "262826" : "Delowar Hossan",
+    "262827" : "Morris Hossan",
+    "262828" : "Mitu",
+
+}
+print(studentid["262826"])
+print(studentid)
+print(studentid.get("262823","Not a valid key"))
