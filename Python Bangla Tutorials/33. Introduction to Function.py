59f599e63d7d488c37194d5c17f40782fe600bc5
Delowar-Designer <delowar.designer@gmail.com>
2023-12-21 09:32:20 +0600

    Introduction to Function

---
+++ Python Bangla Tutorials/33. Introduction to Function.py
@@ -0,0 +1,25 @@
+# introduction to Functon
+a = 20
+b = 10
+sum = a + b
+print("The sum of: ", sum)
+
+def add(x, y):
+    sum = x + y
+    print("Enter the Vealu x and y : ", sum)
+add(20, 20)
+add(41,52)
+
+def addition(T,R,N):
+    sum = T + R + N
+    print("Enter the Vealu: ", sum)
+addition(40, 45, 1)
+
+def sub(x,E):
+    result = x - E
+    print("Enter the",result)
+sub(4, 6)
+
+def message():
+    print("Hello No Parameter")
+message()
