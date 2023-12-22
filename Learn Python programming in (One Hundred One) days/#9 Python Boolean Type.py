4986243097b338f4bb7f93e087e73d05272c7718
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 21:16:37 +0600

    Python Boolean Type

---
+++ Learn Python programming in (One Hundred One) days/#9 Python Boolean Type.py
@@ -0,0 +1,22 @@
+#9 Python Boolean Type
+bool = True
+
+print(type(bool))
+print(bool)
+
+bool1 = False
+
+print(bool1)
+print(type(bool1))
+
+x = 41
+y = 41
+
+print(x == y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+
