6fb3a23de537a14283c445a559eebb27c11127cf
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 22:29:55 +0600

    Python Sequenc Type

---
+++ Learn Python programming in (One Hundred One) days/#12 Python Sequenc Type.py
@@ -0,0 +1,27 @@
+#12 Python Sequenc Type
+# Sequence Types:	list, tuple, range
+li = ['eggs', 'spam', 'bacon', 111]
+
+print(li)
+
+li[0] = "Delowar"
+
+print(li)
+print(type(li))
+
+
+# Tuple
+tup = (5, 10, 15, 20, 25)
+
+print(tup)
+print(type(tup))
+
+
+# Range type Data
+ran = range(10)
+
+for i in ran:
+    print(i)
+    print(type(i))
+print(ran)
+print(type(ran))
