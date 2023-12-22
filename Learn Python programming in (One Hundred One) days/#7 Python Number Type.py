108f6b3a13438c9f951ffa62b32f7d7f78545c00
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 19:37:39 +0600

    Python Number Types

---
+++ Learn Python programming in (One Hundred One) days/#7 Python Number Type.py
@@ -0,0 +1,29 @@
+#7 Python Number Type
+'''
+Numeric Types:	int, float, complex
+x = 20	int
+x = 20.5	float
+x = 1j	complex
+
+x = int(20)	int
+x = float(20.5)	float
+x = complex(1j)	complex
+'''
+# int
+num1 = 455557522335
+
+print(num1)
+print(type(num1))
+
+# float
+Gpa = 4254545454.2777777
+
+print(Gpa)
+print(type(Gpa))
+
+# complex
+rom = 2j
+
+print(rom)
+print(type(rom))
+
