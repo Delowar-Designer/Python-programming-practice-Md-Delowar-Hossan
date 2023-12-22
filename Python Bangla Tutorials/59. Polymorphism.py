55aeb7cbe0cbbba6dca9667a8a5bff5346a1a606
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 06:57:17 +0600

    Polymorphism

---
+++ Python Bangla Tutorials/59. Polymorphism.py
@@ -0,0 +1,13 @@
+# Polymorphism
+# Builtin polymorphic function
+print(len("Welcome to the polymorphism"))
+print(len([10,20,30]))
+
+
+# user defined polymorphic function
+def add(x, y, z=0):
+    return x + y + z
+
+print(add(2, 3))
+print(add(2, 3, 4))
+
