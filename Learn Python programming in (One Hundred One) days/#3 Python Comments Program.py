f692d64b78bf57c94c7bf7e12b4bf776c1349e4a
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 18:16:15 +0600

    Python Comments program

---
+++ Learn Python programming in (One Hundred One) days/#3 Python Comments Program.py
@@ -0,0 +1,8 @@
+#3 Python Comments Program
+# print diye amra  kono jinser putput diye thaky
+print("Python Comments")
+# single line comment
+'''Milti line comment
+Helow Delowar prigramar
+Python Comments'''
+print("Milti line comment")
