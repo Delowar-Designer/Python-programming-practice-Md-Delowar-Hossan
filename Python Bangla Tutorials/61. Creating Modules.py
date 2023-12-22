d3a23912d30a6c704da7ce3c73f01df88b73732f
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 08:07:48 +0600

    Creating Modules in user

---
+++ Python Bangla Tutorials/61. Creating Modules.py
@@ -0,0 +1,9 @@
+# Creating Modules
+'''from math import pow, sqrt
+print(pow(2, 3))
+print(sqrt(3))'''
+
+from Area_Delowar import *
+triangle_area(10,20)
+rectangle_area(10,20)
+
---
+++ Python Bangla Tutorials/Area_Delowar.py
@@ -0,0 +1,6 @@
+def triangle_area(base, height):
+    print(f"Area of Triangle is : {(base * height)/2}")
+    #print(f"Perimeter of Triangle is : {0.5* base * height}")
+
+def rectangle_area(a, b):
+    print(f"Area of rectangle is : {a * b}")
