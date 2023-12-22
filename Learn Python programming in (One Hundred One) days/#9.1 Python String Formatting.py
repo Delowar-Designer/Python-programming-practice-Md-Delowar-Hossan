0764ecc9e4729748c1425679ffb72dde7a28dfb3
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 21:28:25 +0600

    Python String Formattig

---
+++ Learn Python programming in (One Hundred One) days/#9.1 Python String Formatting.py
@@ -0,0 +1,12 @@
+#9.1 Python String Formatting
+num1 = 20
+num2 = 30
+
+print(f"this is my super number {num1 + num2} ")
+print(f"this is my super number", num1 + num2)
+print("this is my super number", num1 + num2)
+print(f"this is my super number {52 + 22}",{5 + 5})
+
+username = "Delowar"
+Roll = "602826"
+print(f"my name is {username} & my class roll is {Roll}")
