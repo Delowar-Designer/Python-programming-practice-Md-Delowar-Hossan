5b6ba530abf18c8937d942e6f5fab3611a6203f0
Delowar-Designer <delowar.designer@gmail.com>
2023-12-22 18:59:06 +0600

    Python Data Type

---
+++ Learn Python programming in (One Hundred One) days/#6 Python Data Type.py
@@ -0,0 +1,25 @@
+#6 Python Data Type
+""""Built-in Data Types
+In programming, data type is an important concept.
+
+Variables can store data of different types, and different types can do different things.
+
+Python has the following data types built-in by default, in these categories:
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType"""
+
+
+Roll = "Python Data Type"
+class1 = "Python Data Type class1"
+numbe = 1444
+
+print(Roll)
+print(class1)
+print(numbe)
