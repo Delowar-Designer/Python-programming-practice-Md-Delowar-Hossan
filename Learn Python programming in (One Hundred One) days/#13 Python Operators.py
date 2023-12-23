ee8ce6f8dd937b8afe4e18340716cc250fb97f0f
Delowar-Designer <delowar.designer@gmail.com>
2023-12-23 06:02:41 +0600

    Python Operators

---
+++ Learn Python programming in (One Hundred One) days/#13 Python Operators.py
@@ -0,0 +1,25 @@
+#13 Python Operators
+#------Python Arithmetic Operators-----
+# Addition
+a = 5
+b = 2
+print(a + b)
+
+# Subtraction
+print(a - b)
+
+# Multiplication
+print(a * b)
+
+# Division
+print(a / b)
+
+# Modulus
+print(a % b)
+
+# Exponentiation
+print(a ** b)
+
+# Floor division
+print(a // b)
+
