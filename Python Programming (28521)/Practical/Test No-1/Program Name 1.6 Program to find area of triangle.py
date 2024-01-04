35109266a226072b4fe74375ab4279ad5097773d
Delowar-Designer <delowar.designer@gmail.com>
2024-01-04 22:40:46 +0600

    Program to Find Area of Triangle

---
+++ Python Programming (28521)/Practical/Test No-1/Program Name 1.6 Program to find area of triangle.py
@@ -0,0 +1,6 @@
+# Program Name 1.6 Program to find area of triangle
+height = float(input("Enter Height = "))
+width = float(input("Enter Width = "))
+area = height * width * 0.5
+print("Triangle Area = ",area)
+
