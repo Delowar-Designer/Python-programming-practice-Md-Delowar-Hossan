323c98e1f78f9d2be28c5567d4e17c9c9456bde0
Delowar-Designer <delowar.designer@gmail.com>
2024-01-04 22:52:35 +0600

    Praam for finding rectangles

---
+++ Python Programming (28521)/Practical/Test No-1/Program Name 1.7 Program for finding rectangles.py
@@ -0,0 +1,11 @@
+# Program Name 1.7 Program for finding rectangles
+length = int(input("Enter the length = "))
+width = int(input("Enter the width = "))
+area = length * width
+print("The Rectangle Area is = ",area)
+
+
+length = float(input("Enter the Float Number length = "))
+width = float(input("Enter the Float Number width = "))
+area = length * width
+print("The Float Number of Rectangle Area is = ",area)
