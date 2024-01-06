978c43be30cef678f0843ce60840b7fd14d5cd99
Delowar-Designer <delowar.designer@gmail.com>
2024-01-06 06:57:38 +0600

    Program to convert Fahrenheit ti Celsius

---
+++ Python Programming (28521)/Practical/Test No-1/Program Name 1.9 Program to convert Fahrenheit to Celsius.py
@@ -0,0 +1,4 @@
+# Program to convert Fahrenheit to Celsius
+fahrenheit = float(input("Enter The Fahrenheit Temperature = "))
+celsius = (fahrenheit - 32) / 1.8
+print("Fahrenheit to Celsius Temperature = ",celsius)
