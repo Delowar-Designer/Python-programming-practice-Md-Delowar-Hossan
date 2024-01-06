fe0f47bdb2d6bd19fdb4128354d2f8db9f0f8255
Delowar-Designer <delowar.designer@gmail.com>
2024-01-06 06:48:58 +0600

    Program to conert centigrade temperature to Fahrenheit

---
+++ Python Programming (28521)/Practical/Test No-1/Programm Name 1.8 Program to convert centigrade temperature to fahrenheit.py
@@ -0,0 +1,4 @@
+# Program to convert centigrade temperature to fahrenheit
+celsis = float(input("Enter the Celsius Temperature = "))
+fahrenheit = (celsis * 1.8) + 32
+print("The Celsius to Fahrenheit Temperature = ", fahrenheit)
