7b96686dcd50b1e60546031b6bab09287d2a47bd
Delowar-Designer <delowar.designer@gmail.com>
2024-01-06 07:37:50 +0600

    Prigram to Determine Even/Odd Eit a Number input

---
+++ Python Programming (28521)/Practical/Test No -2/Program Name 2.2 Program to determine even/odd with a number input.py
@@ -0,0 +1,7 @@
+# Program to determine even/odd with a number input
+# Check given Number is odd / even
+num = int(input("Enter Number = "))
+if(num % 2):
+    print("Odd Number ")
+else:
+    print("Even Number ")
