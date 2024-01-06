06b72007a061f623a10148899eb6354f62ddc797
Delowar-Designer <delowar.designer@gmail.com>
2024-01-06 07:49:47 +0600

    Program  to Determine Positive/negative With a Number input

---
+++ Python Programming (28521)/Practical/Test No -2/Program Name 2.3 Program to determine positive/negative with a number input.py
@@ -0,0 +1,8 @@
+# Program to determine positive/negative with a number input
+num = int(input("Enter Number = "))
+if(num > 0):
+    print(num,"is a positive number")
+elif(num == 0):
+    print(num,"is a Zero")
+else:
+    print(num,"is a Negativ number")
