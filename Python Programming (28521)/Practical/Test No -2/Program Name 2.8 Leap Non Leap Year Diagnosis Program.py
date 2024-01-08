c369b45147d8b361c4d5756bad8adbf8ec88e858
Delowar-Designer <delowar.designer@gmail.com>
2024-01-08 09:50:55 +0600

    Leap Non Leap Year Diagnosis Program

---
+++ Python Programming (28521)/Practical/Test No -2/Program Name 2.8 Leap Non Leap Year Diagnosis Program.py
@@ -0,0 +1,13 @@
+# Leap Non Leap Year Diagnosis Program
+# Defult Function to inplement conditions to check leap year
+
+def CheckLeap(Year):
+    # Checking if the given year is leap year
+    if((Year % 400 == 0) or (Year % 100!= 0) and (Year % 4 == 0)):
+        print("Given Year is a leap Year") # Else it is not a leap year
+    else:
+        print("Given year is not a leap Year")
+# Taking an input year from user
+Year = int(input("Enter the number: "))
+# Printg Result
+CheckLeap(Year)
