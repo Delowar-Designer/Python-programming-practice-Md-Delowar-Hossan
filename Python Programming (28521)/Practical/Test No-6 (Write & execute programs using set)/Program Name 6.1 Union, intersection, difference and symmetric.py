7b8deb40e6732f45a325ee1fb48da9a98d7fe07d
Delowar-Designer <delowar.designer@gmail.com>
2024-01-19 18:53:32 +0600

    Program Name 6.1 Union, intersection, difference and symmetric

---
+++ Python Programming (28521)/Practical/Test No-6 (Write & execute programs using set)/Program Name 6.1 Union, intersection, difference and symmetric.py
@@ -0,0 +1,14 @@
+# Program Name 6.1 Union, intersection, difference and symmetric set operations in Python
+# Program to perform different set operations
+# as we do in mathematics
+# sets are define
+A = {0, 2, 4, 6, 8}
+B = {1, 2, 3, 4, 5}
+# union
+print("Union :", A | B)
+# intersection
+print("Intersection :", A & B)
+# difference
+print("Difference :", A - B)
+# symmetric difference
+print("Symmetric difference :", A ^ B)
