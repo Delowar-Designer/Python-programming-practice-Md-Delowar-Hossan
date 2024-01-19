016c5cf6dc98ceb4a27d1c3013998f879e65b468
Delowar-Designer <delowar.designer@gmail.com>
2024-01-19 16:44:36 +0600

    Write a program to find the sum of the elemens of a list

--- Python Programming (28521)/Practical/Test No-4 (Write & execute programes using lists array)/Program Name 4.7 Write a program to find the sum of the elements of a list.py
+++ Python Programming (28521)/Practical/Test No-4 (Write & execute programes using lists array)/Program Name 4.7 Write a program to find the sum of the elements of a list.py
@@ -1,1 +1,12 @@
 # Write a program to find the sum of the elements of a list
+items = []
+n = int(input("How Many Input in List= "))
+for i in range(n):
+    value = input("Enter Item= ")
+    items.append(value)
+sum = 0
+for i in items:
+    sum = sum + int(i)
+print("Summation of List= ",sum)
+
+
