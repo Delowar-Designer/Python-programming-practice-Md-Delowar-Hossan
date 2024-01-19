4cfff9fdbc920c4c354b77aa6dfda8cef136b0f9
Delowar-Designer <delowar.designer@gmail.com>
2024-01-19 19:14:41 +0600

    MT dictionary creation program using Python

---
+++ Python Programming (28521)/Practical/Test No-7 (Write & execute programs using a dictionary)/Program Name 7.1 MT dictionary creation program using Python.py
@@ -0,0 +1,6 @@
+# MT dictionary creation program using Python
+# Creating Dictionaries With {} And Dict()
+my_dict_one = {} # create empty dict with curly brackets
+my_dict_two = dict() # create empty dict with built-in dict()
+print(my_dict_one)
+print(my_dict_two)
