7e0e77eb8471fd39d0595151bbbeb942f06b3cb1
Delowar-Designer <delowar.designer@gmail.com>
2024-01-19 18:19:56 +0600

    Program to find the largest number in a list using the length function

---
+++ Python Programming (28521)/Practical/Test No- 5 (Write & execute programs using user-defined functions)/Program Name 5.3.py
@@ -0,0 +1,3 @@
+# Program to find the largest number in a list using the length function
+valuesList = [222,333,444,555,666,668,2,1]
+print("Mazimum Value in List = ",max(valuesList, key = lambda value:int(value)))
