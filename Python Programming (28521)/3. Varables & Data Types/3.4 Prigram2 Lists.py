36a6b02acdf28326a7cf253bc983a38b2ab5a296
Delowar-Designer <delowar.designer@gmail.com>
2024-01-23 23:18:41 +0600

    Lists

---
+++ Python Programming (28521)/3. Varables & Data Types/3.4 Prigram2 Lists.py
@@ -0,0 +1,11 @@
+# Lists
+#!/usr/bin/python
+list = ['Mahi', 786, 2.23, 'Delowar', 70.2]
+tinylist = [123, 'Delowar']
+
+print(list) # Prints conplete list
+print(list[0]) # Prints first element of the list
+print(list[1:3]) # Prints elements starting from 2nd till 3rd
+print(list[2:]) # Prints elements starting from 3rd element
+print(tinylist * 2) # Prints list two times
+print(list + tinylist) # Prints concatenated lists
