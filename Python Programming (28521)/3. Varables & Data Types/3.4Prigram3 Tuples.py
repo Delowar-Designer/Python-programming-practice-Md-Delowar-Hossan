0554d99eea2bd5d09539a0571a99e0cef1554009
Delowar-Designer <delowar.designer@gmail.com>
2024-01-23 23:36:06 +0600

    Tuples

---
+++ Python Programming (28521)/3. Varables & Data Types/3.4Prigram3 Tuples.py
@@ -0,0 +1,11 @@
+# Tuples
+#!/usr/bin/python
+tuple = ('Mitu', 786, 2.23, 'Delowar', 70.2)
+tinytuple = (123, 'Mitu')
+
+print(tuple) # Prints complete tuple
+print(tuple[0]) # Prints first element of the tuple
+print(tuple[1:3]) # Prints elements starting from 2nd till 3rd
+print(tuple[2:]) # Prints elements starting from 3rd element
+print(tinytuple *2) # Prints tuple two times
+print(tuple + tinytuple) # Prints concatenated tuple
