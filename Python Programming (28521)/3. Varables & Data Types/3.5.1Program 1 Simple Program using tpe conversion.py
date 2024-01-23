7b2dc8ccc25455b2e0f907cea8b1f9092b11a702
Delowar-Designer <delowar.designer@gmail.com>
2024-01-24 00:50:42 +0600

    Fahrenheit temperature conversion celsius in Program

---
+++ Python Programming (28521)/3. Varables & Data Types/3.5.1Program 1 Simple Program using tpe conversion.py
@@ -0,0 +1,5 @@
+# Simple program using type conversion
+# Fahrenheit temperature conversion celsius in program
+fahrenheit = float(input("Please enter the fahrenheit temperature:")) # type conversion
+celsius = (fahrenheit-32)/1.8
+print('%0.1f degree fahrenheit is+% 0.1f degrees Celsius' %(fahrenheit,celsius))
