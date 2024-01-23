a24d4fe971dcf6051f0906d60ec8707d989bd1ad
Delowar-Designer <delowar.designer@gmail.com>
2024-01-23 17:15:08 +0600

    Single Line and Multiline comments

---
+++ Python Programming (28521)/2. Basics of Python/2.3 Program 2 Comments in Python.py
@@ -0,0 +1,15 @@
+# Commwnts in Python
+# This is a single line comment
+print("Hello World")
+print("Hello, World") # This is a comment
+
+"""
+This is a comment
+    written in
+    more than just one line
+"""
+print("Hello World!")
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
