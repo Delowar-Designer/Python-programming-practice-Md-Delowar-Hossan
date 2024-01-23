651e22f94068dae7a57967831d44d7a23faba686
Delowar-Designer <delowar.designer@gmail.com>
2024-01-24 00:41:28 +0600

    Data type conversion

---
+++ Python Programming (28521)/3. Varables & Data Types/3.5 Program 1 Data type conversion.py
@@ -0,0 +1,24 @@
+# Data type conversion
+# String to Integer Conversion
+int("123")
+
+# float to Lnteger Conversion
+int(12.3)
+
+int("123a")
+
+# String to float Conversion
+float("123.456")
+
+# Integer to float Conversion
+float(123)
+
+float("123.456")
+
+int(123.456)
+
+int("123.456")
+
+str(123)
+
+print("Float = " + str(10.5)+ ", Integer = " + str(50))
