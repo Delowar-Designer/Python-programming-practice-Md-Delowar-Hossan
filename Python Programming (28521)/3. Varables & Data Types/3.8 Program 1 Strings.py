aab8ccb5e44d1e123c37cb5872c45dc2f2ca12e1
Delowar-Designer <delowar.designer@gmail.com>
2024-01-23 22:56:08 +0600

    Strings

---
+++ Python Programming (28521)/3. Varables & Data Types/3.8 Program 1 Strings.py
@@ -0,0 +1,11 @@
+# Strings
+# !/usr/bin/python
+str = 'Md Delowar Hossan!'
+print(str) # Prints complete string
+print(str[0]) # Prints first character of the string
+print(str[2:5]) # Prints characters starting from 3rd to 5th
+print(str[2:]) # Prints string starting from 3rd character
+print(str*2) # Prints string two times
+print(str + "Welcome") # Prints concatenated string
+
+
