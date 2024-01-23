cd407541183916e3ecb0453e0c328bdee20e3331
Delowar-Designer <delowar.designer@gmail.com>
2024-01-23 16:55:43 +0600

    Quolation in Python

---
+++ Python Programming (28521)/2. Basics of Python/2.3 Program 1 Quolation in Python.py
@@ -0,0 +1,11 @@
+# Quolation in Python
+print("double quotation")
+print('here is the single')
+print("single quote - ' in a double quoted string")
+print('double quotation - " in a single quoted string')
+print("here comes the escaped quotes - \"")
+print('here is the single one - \'')
+print(""" This is a paragraph. This is 
+        .made up of multiple lines and sentences.
+        This book is written by Md. Delowar Hossan and Mitu
+        Both are Graduated from DUET""")
