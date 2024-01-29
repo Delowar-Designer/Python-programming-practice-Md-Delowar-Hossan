fa0c3fbb3ce5ecb210a768b768a8aa9f636af727
Delowar-Designer <delowar.designer@gmail.com>
2024-01-29 09:40:47 +0600

    Operators Precedence

---
+++ Python Programming (28521)/4. Python Operators/4.4 Programming 1  Operators Precedence.py
@@ -0,0 +1,14 @@
+#  Operators Precedence
+a = 20
+b = 10
+c = 15
+d = 5
+e = 0
+e = (a+b)*c/d
+print("Value of (a+b)*c/d is",e)
+e = ((a+b)*c)/d;
+print("Value of (a+b)*c/d is",e)
+e = (a+b)*(c/d);
+print("Value of (a+b)*(c/d) is",e)
+e = a + (b*c)/d;
+print("Value of (a*b)/d is",e)
