d1ee0763a06bc6b8b67e526e15ed95ceb5fd5988
Delowar-Designer <delowar.designer@gmail.com>
2024-01-29 07:19:34 +0600

    Logical expression

---
+++ Python Programming (28521)/4. Python Operators/4.2 Programming 3 Logical expression.py
@@ -0,0 +1,12 @@
+#  Logical expression
+print(5 == 5 and 6 == 6)
+print(1 == 2 and 5 == 5)
+print(5 == 5 or 6 == 6)
+print(5 == 6)
+print(not 5 == 6)
+x = 5
+y = 6
+z = 4
+print((x > y) and (x < y))
+print((x > y) or (x < y))
+print(((x > y) or (x < y)) >= 5)
