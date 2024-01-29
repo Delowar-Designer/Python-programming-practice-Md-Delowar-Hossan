7d5f820db462e119a03573c32e157725e41d384e
Delowar-Designer <delowar.designer@gmail.com>
2024-01-29 06:40:31 +0600

    Type use of functions

--- Python Programming (28521)/3. Varables & Data Types/3.9 Program 1 Type Finceon.py
+++ Python Programming (28521)/3. Varables & Data Types/3.9 Program 1 Type Finceon.py
@@ -1,12 +1,16 @@
-# Type funcon
+# Type Use of functions
 a = 1
 type(a)
+print(type(a))
 
 b = 3.1416
 type(b)
+print(type(b))
 
 c = "Md Delowar Hossan"
 type(c)
+print(type(c))
 
 d = True
 type(d)
+print(type(d))
