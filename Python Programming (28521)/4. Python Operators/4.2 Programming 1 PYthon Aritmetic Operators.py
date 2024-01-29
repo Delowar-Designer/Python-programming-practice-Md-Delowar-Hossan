945cb0cd00e6effbfad35d618eecfb9a76b23c8f
Delowar-Designer <delowar.designer@gmail.com>
2024-01-29 06:58:32 +0600

    Python Aritmetic Operators

---
+++ Python Programming (28521)/4. Python Operators/4.2 Programming 1 PYthon Aritmetic Operators.py
@@ -0,0 +1,14 @@
+# PYthon Aritmetic Operators
+a = 10
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+print(7//2)
+print(5.0/10.0)
+print(-20+5)
+print((10+5)*2)
