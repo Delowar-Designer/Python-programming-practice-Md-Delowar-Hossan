a3eb257b4c9b4a39663e7caedb3fca047586ff46
Delowar-Designer <delowar.designer@gmail.com>
2024-01-29 10:31:47 +0600

    Operators precedence & Associativity in Python

---
+++ Python Programming (28521)/4. Python Operators/4.5 Programming 1 Operators Precedence & Associativity in Python.py
@@ -0,0 +1,7 @@
+# OPerators Precedence & Associativity in Python
+print(2 ** 2 ** 2)
+print(2 ** 2 ** 2 ** 2)
+print((2 ** 2 ** 2) ** 2)
+print(3 ** 3 ** 3)
+print((3 ** 3) ** 3)
+print(3 ** (3 ** 3))
