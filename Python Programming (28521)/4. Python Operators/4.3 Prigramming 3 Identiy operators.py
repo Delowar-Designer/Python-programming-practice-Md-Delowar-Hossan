c9e8f44a4588a61499611182863f1264911f5384
Delowar-Designer <delowar.designer@gmail.com>
2024-01-29 08:28:38 +0600

    Identiy operators

---
+++ Python Programming (28521)/4. Python Operators/4.3 Prigramming 3 Identiy operators.py
@@ -0,0 +1,15 @@
+# Identiy operators
+a = 'bangladesh'
+b = 'bash'
+if a is b:
+    print('a is b')
+elif a is b:
+    print('a is not b')
+    
+a = 'bangladesh'
+b = 'bash'
+if a is not b:
+    print('a is not b')
+
+elif a is b:
+    print('a is b')
