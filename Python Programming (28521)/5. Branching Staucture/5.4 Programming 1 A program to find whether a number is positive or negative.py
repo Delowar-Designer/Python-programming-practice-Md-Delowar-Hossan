5c7a485be9342648e4ddc41581a985de07ac6991
Delowar-Designer <delowar.designer@gmail.com>
2024-01-30 13:06:15 +0600

    A program to find whether a number is positive or negative

---
+++ Python Programming (28521)/5. Branching Staucture/5.4 Programming 1 A program to find whether a number is positive or negative.py
@@ -0,0 +1,6 @@
+# A program to find whether a number is positive or negative
+a = int(input("Enter the number "))
+if a>0:
+    print(a,"is Positive Number.")
+if 0>a:
+    print(a,"is Negative Number.")
