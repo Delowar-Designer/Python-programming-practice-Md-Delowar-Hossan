992719ee941ec7056075c6d1966cd1360f5d16a2
Delowar-Designer <delowar.designer@gmail.com>
2024-01-30 17:24:50 +0600

    range Function with for loop

---
+++ Python Programming (28521)/6. Loopping Structure/6.2 Programming Program to wish friends stored in a list.py
@@ -0,0 +1,6 @@
+# Program to wish friends stored in a list
+# list of friends
+friends = ['Kadir','Azad','Foysl','Riad']
+for name in friends:
+    print("Happy New year: ",name)
+    print("Thanks a lot")
