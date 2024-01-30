45ad2fa0a9f2343e649689e5dc253a3296df3341
Delowar-Designer <delowar.designer@gmail.com>
2024-01-30 12:38:21 +0600

    Python if statements

---
+++ Python Programming (28521)/5. Branching Staucture/5.2 Programming 1 Python if statements.py
@@ -0,0 +1,9 @@
+# Python if Statements
+num = 5
+if num > 0:
+    print(num, "is a positive number.")
+print("This is always printed.")
+num = - 5
+if num > 0:
+    print(num, "is a positive number.")
+print("This is also alwayd printed.")
