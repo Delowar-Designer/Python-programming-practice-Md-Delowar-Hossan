4f46241c848cb09409a7649ce61f5555d1f13c67
Delowar-Designer <delowar.designer@gmail.com>
2024-01-30 15:10:27 +0600

    Grade calculation program with marks input

---
+++ Python Programming (28521)/5. Branching Staucture/5.4 Programming 5 Grade calculation program with marks input.py
@@ -0,0 +1,12 @@
+# Grade calculation program with marks input
+n = int(input("Enter number Marks: "))
+if n>=90:
+    print("A+")
+elif n>=80:
+    print("A")
+elif n>=70:
+    print("B")
+elif n>=60:
+    print("C")
+else:
+    print("F")
