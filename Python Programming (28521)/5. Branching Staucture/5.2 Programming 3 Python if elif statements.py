05c8d8f035acdcf2d0738536bffd644d1a2fdc9a
Delowar-Designer <delowar.designer@gmail.com>
2024-01-30 12:57:41 +0600

    Python if elif statements

---
+++ Python Programming (28521)/5. Branching Staucture/5.2 Programming 3 Python if elif statements.py
@@ -0,0 +1,9 @@
+# Python if elif statements
+num =- 12
+# num = 5 and num = 0 positive number and Negative number
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
