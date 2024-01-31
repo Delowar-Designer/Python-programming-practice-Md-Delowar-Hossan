86aa654b3fc2eb891cbf6dc96a525fb93950334c
Delowar-Designer <delowar.designer@gmail.com>
2024-01-31 12:03:21 +0600

    Neated while loop statement

---
+++ Python Programming (28521)/6. Loopping Structure/6.4 Programming 3 Neated while loop statement.py
@@ -0,0 +1,16 @@
+# Neated while loop statement
+'''
+
+while Condition:
+    while Condition:
+        statement(s)
+    statement(s)
+    '''
+b = 0
+while b <= 2:
+    x = 0
+    print(b)
+    while x <= 2:
+        print(x)
+        x += 1
+    b += 1
