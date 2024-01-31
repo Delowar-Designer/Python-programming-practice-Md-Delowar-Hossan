483c75111e3bdb44fef5467c1d4eb2b554bc2c4d
Delowar-Designer <delowar.designer@gmail.com>
2024-01-31 12:17:54 +0600

    Neated while loop statement

---
+++ Python Programming (28521)/6. Loopping Structure/6.4 Programming 4 Neated while loop statement.py
@@ -0,0 +1,13 @@
+# Neated while loop statement
+i = 2
+while(i < 20):
+    j = 2
+    while(j <= (i/j)):
+        if not (i%j):
+            break
+        j = j + 1
+    if(j>i/j):
+        print(i, "is prime")
+    i = i + 1
+
+
