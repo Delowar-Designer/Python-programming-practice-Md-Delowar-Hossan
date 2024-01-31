a7d2d4990fbbbb7228a15fadd38af283ca67fb3a
Delowar-Designer <delowar.designer@gmail.com>
2024-01-31 12:27:33 +0600

    A program that takes an integer input and prints the name of that integer using a for loop

---
+++ Python Programming (28521)/6. Loopping Structure/6.5 Programming 1 A program that takes an integer input and prints the name of that integer using a for loop.py
@@ -0,0 +1,4 @@
+# A program that takes an integer input and prints the name of that integer using a for loop
+number = int(input("Enter the number: "))
+for x in range(1,11):
+    print(number,"*",x,"=",number*x)
