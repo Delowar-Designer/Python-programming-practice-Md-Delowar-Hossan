063a642c6e92c01dc0c4414a171c6216b0875100
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 13:00:19 +0600

    List & floating Point type data

---
+++ Python Programming (28521)/7. List Structure/7.2 Programming 2 List & floating Point type data.py
@@ -0,0 +1,8 @@
+# List & floating Point type data
+Numbers = [5.4, 3.7, 10.4, 25.6, 33.7, 7.18, 172.34, 97.99]
+print(Numbers)
+print(Numbers[3])
+Numbers.sort()
+print(Numbers)
+Numbers.reverse()
+print(Numbers)
