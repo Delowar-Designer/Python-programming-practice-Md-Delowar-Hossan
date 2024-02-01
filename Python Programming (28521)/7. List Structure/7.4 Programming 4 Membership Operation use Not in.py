3d72c0670279930b143b185a5fab14773c6d32de
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 17:44:56 +0600

    Membership Operation use Not in

---
+++ Python Programming (28521)/7. List Structure/7.4 Programming 4 Membership Operation use Not in.py
@@ -0,0 +1,7 @@
+# Membership Operation use Not in
+List1 = ["Delowar",960021,"Mitu",23022015]
+List2 = [19072017,"Mahdee","Tahsin"]
+print("Samsul" not in List1)
+print("Mitu" not in List2)
+print("Delowar" not in List1)
+print(960021 not in List2)
