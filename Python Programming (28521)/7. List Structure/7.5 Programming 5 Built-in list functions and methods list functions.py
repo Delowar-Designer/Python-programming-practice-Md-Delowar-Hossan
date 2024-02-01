9d7d826651529bde9c3b6e7d0597c9d371819dc6
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 19:16:13 +0600

    Built-in list functions and methods list functions

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 5 Built-in list functions and methods list functions.py
@@ -0,0 +1,12 @@
+# Built-in list functions and methods list functions
+# empty list
+print(list())
+# vowel string
+vowelString = 'aeiou'
+print(list(vowelString))
+# vawel Tuple
+vawelTuple = ('a','e','i','o','u')
+print(list(vawelTuple))
+# Vowel list
+vowelList = ['a', 'e', 'i', 'o', 'u']
+print(list(vowelList))
