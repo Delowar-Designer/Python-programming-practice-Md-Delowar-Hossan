46c91962e596cdcad636c391fa27d84285537c54
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 13:11:11 +0600

    Python lists can be created in conjunction with Ekaji Steam

---
+++ Python Programming (28521)/7. List Structure/7.2 Programming 3 Python lists can be created in conjunction with Ekaji Steam.py
@@ -0,0 +1,5 @@
+# Python lists can be created in conjunction with Ekaji Steam
+List_Name = ['Abdul Kadir','Md.Wahid Ullah','Mehr Ali','Md.Shohei','Ahmed Bappi']
+print(List_Name[0],',',List_Name[4],',',List_Name[2])
+print(List_Name[3])
+print(List_Name[1])
