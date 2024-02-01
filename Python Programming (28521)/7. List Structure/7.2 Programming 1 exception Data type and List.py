65ded6d95e51390979bb6fc4e645dcadce22cb35
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 13:21:36 +0600

    exception Data type and List

---
+++ Python Programming (28521)/7. List Structure/7.2 Programming 1 exception Data type and List.py
@@ -0,0 +1,7 @@
+# exception Data type and List
+My_List = ['Abdul Kadir','CSE','DUET','154054','3.54']
+print("Name:",My_List[0])
+print("Department:",My_List[1])
+print("University:",My_List[2])
+print("Std_Id:",My_List[3])
+print("CGPA:",My_List[4])
