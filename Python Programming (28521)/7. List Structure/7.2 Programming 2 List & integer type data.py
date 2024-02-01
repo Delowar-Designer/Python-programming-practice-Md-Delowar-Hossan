bc8051405d6bb06893d713411537b23ca7710cc8
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 11:16:47 +0600

    List & integer type data

---
+++ Python Programming (28521)/7. List Structure/7.2 Programming 2 List & integer type data.py
@@ -0,0 +1,11 @@
+# List & integer type data
+Numbers = [5,7,4,6,3,18,1,9]
+print(Numbers)
+print(Numbers[5])
+print(Numbers[3])
+Numbers.sort()
+print(Numbers)
+Numbers.reverse()
+print(Numbers)
+
+
