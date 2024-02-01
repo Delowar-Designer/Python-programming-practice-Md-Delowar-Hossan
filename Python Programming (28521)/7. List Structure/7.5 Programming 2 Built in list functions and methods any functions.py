4354f5e6d82211f36a81ce4e6321744489513fe6
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 18:40:41 +0600

    Built-in list functions and methods any functions

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 2 Built in list functions and methods any functions.py
@@ -0,0 +1,11 @@
+# Built-in list functions and methods any functions
+list = [1,2,3,4,5]
+print(any(list))
+list = [0, False]
+print(any(list))
+list = [1,3,4,0]
+print(any(list))
+list = [0,False,5]
+print(any(list))
+list = []
+print(any(list))
