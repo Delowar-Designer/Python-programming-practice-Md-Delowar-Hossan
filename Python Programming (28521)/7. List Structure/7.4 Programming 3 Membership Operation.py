735fc789a5616fc88f83ba4434bedeae82179275
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 17:40:07 +0600

    Membership Operation

---
+++ Python Programming (28521)/7. List Structure/7.4 Programming 3 Membership Operation.py
@@ -0,0 +1,7 @@
+# Membership Operation
+List1 = ["Delowar",960021,"Mitu",23022015]
+List2 = [19072017,"Mahdee","Tahsin"]
+print("Samsul" in List1)
+print("Mitu" in List2)
+print("Delowar" in List1)
+print(960021 in List2)
