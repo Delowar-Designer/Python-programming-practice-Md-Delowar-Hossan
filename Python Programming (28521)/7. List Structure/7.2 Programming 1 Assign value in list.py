6a1fb07f8f39135bfd9f6c333546929b7b0491aa
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 11:00:13 +0600

    Assign value in list

---
+++ Python Programming (28521)/7. List Structure/7.2 Programming 1 Assign value in list.py
@@ -0,0 +1,7 @@
+# Assign value in list
+Address = ['Abdul kadir', 'CSE', 'DUET','https://www.facebook.comm/Delowar.Designer']
+name, dept,university, contact = Address
+print(name)
+print(dept)
+print(university)
+print(contact)
