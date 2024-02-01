490041c3ea07a571e0570287a07375a935bf7a40
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 14:40:31 +0600

    Inserting new item in the list

---
+++ Python Programming (28521)/7. List Structure/7.3 Programming 2 Inserting new item in the list.py
@@ -0,0 +1,8 @@
+# Inserting new item in the list
+b = ['Mahi',587,'COBOL',3.1416,'Samsul',96002]
+b.append('Mahdee')
+print(b)
+b.insert(3,'Tonny')
+print(b)
+b.extend('Munny''Sumaiya''Tahsin')
+print(b)
