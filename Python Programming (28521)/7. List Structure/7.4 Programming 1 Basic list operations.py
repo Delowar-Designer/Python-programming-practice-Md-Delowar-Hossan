3fc38abb41fd124eb6bf6d5348205b7b10f75e40
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 17:26:28 +0600

    asic list operations

---
+++ Python Programming (28521)/7. List Structure/7.4 Programming 1 Basic list operations.py
@@ -0,0 +1,8 @@
+# asic list operations
+List1 = ["Mahbub",960021,"Mahi",23022015]
+List2 = [19072017,"Mahdee","Tahsin"]
+List3 = List1 + List2
+print(List3)
+List4 = List1 + [39088786,"Delowar","Hossan"]
+print(List4)
+print(List1 +[234245,"Mitu","Joty"])
