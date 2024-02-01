bfa044abc4aaba9ea9af5c2f6182fe4ac68e717f
Delowar-Designer <delowar.designer@gmail.com>
2024-02-01 09:45:25 +0600

    Show the use of its form with the list

---
+++ Python Programming (28521)/6. Loopping Structure/6.7 Programming  1 Show the use of its form with the list.py
@@ -0,0 +1,8 @@
+# Show the use of its form with the list
+# Program to wish friends stored in a list
+# list of frineds
+
+friends = ['Kadir', 'Azad', 'Foysal', 'Riad']
+for name in friends:
+    print("Happy New year: ",name)
+    print("Thanks a lot")
