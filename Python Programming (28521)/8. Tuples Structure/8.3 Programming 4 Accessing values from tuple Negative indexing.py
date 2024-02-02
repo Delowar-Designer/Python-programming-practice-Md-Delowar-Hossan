559b9e73c1555ae86c842808a8599ce9b0dbee28
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 21:41:04 +0600

    Accessing values from tuple Negative indexing

---
+++ Python Programming (28521)/8. Tuples Structure/8.3 Programming 4 Accessing values from tuple Negative indexing.py
@@ -0,0 +1,9 @@
+# Accessing values from tuple Negative indexing
+Tuple = ('Delowar','Hossan','Joty','Motu','Majudur',602826,4.82)
+print(Tuple[0])
+print(Tuple[-1])
+print(Tuple[-4:-1])
+print(Tuple[:-3])
+print(Tuple[-2:])
+print(Tuple)
+
