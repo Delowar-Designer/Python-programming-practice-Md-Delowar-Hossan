5d6b6901bc062dc105eed27604d700425ac7909e
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 23:07:41 +0600

    Built-in tuple functions len

---
+++ Python Programming (28521)/8. Tuples Structure/8.6 Programming 4 Built-in tuple functions len.py
@@ -0,0 +1,7 @@
+# Built-in tuple functions len
+testTuple = ()
+print(testTuple,'length is',len(testTuple))
+testTuple = (1,2,3)
+print(testTuple,'length is',len(testTuple))
+testTuple = ("Delowar")
+print(testTuple,'length is',len(testTuple))
