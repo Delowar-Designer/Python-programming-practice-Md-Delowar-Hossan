05a3c259c3a2fc572a518af55a28be4d5a623f3f
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 09:25:04 +0600

    Built-in list functions and methods min functions

---
+++ Python Programming (28521)/7. List Structure/7.5 Programing 7 Built-in list functions and methods min functions.py
@@ -0,0 +1,6 @@
+# Built-in list functions and methods min functions
+# using min(arg1, * args)
+print('Maximum is:',min(17,3,22,5,47))
+# using min(iterable)
+num = [11,3,2,8,5,10,6,15]
+print('Maximum is:',min(num))
