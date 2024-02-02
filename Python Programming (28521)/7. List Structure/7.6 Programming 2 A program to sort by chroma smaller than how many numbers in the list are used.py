a767fa0bc9fe3f613dc236f2a8e41e217881e8dd
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 16:57:55 +0600

    A program to sort by chroma smaller than how many numbers in the list are used

---
+++ Python Programming (28521)/7. List Structure/7.6 Programming 2 A program to sort by chroma smaller than how many numbers in the list are used.py
@@ -0,0 +1,4 @@
+# A program to sort by chroma smaller than how many numbers in the list are used
+my_list = [1,5,2,6,3,7,4]
+my_list.sort(reverse = 'true')
+print("sorted list in descending:",my_list)
