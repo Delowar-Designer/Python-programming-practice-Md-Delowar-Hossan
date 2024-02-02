59e9342e73e40e7aed16380cbe4829fcece4d8e8
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 23:17:11 +0600

    Built-in tuple functions tuple

---
+++ Python Programming (28521)/8. Tuples Structure/8.6 Programming 5 Built-in tuple functions tuple.py
@@ -0,0 +1,14 @@
+# Built-in tuple functions tuple
+# empty tuple
+print(tuple())
+# vowel string
+vowelString = 'aeiou'
+print(tuple(vowelString))
+
+# vowel tuple
+vowelTuple = ('a','e','i','o','u')
+print(tuple(vowelTuple))
+
+# vowel tuple
+vowelTuple = ('Delowar')
+print(tuple(vowelTuple))
