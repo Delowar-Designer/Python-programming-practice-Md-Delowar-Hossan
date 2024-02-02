cdbba15010f8109525adef3400232bec45c3bc78
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 14:45:38 +0600

    List Methods Use in append

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 10 List Methods Use in append.py
@@ -0,0 +1,7 @@
+# List Methods Use in append
+List1 = ['Mahi','Mahdee','Suhaima']
+List1.append('Tahsin')
+print('Updated List is: ',List1)
+List2 = ['Tonny','Munny','Sumaiya']
+List1.append(List2)
+print('Updated list is:',List1)
