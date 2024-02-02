ad4afc78fe0dc2a03b799132bacbadc2fb60af3e
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 09:33:22 +0600

    Built-in list functions and methods sorted functions

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 8 Built-in list functions and methods sorted functions.py
@@ -0,0 +1,6 @@
+#  Built-in list functions and methods sorted functions
+List1 = ['e','a','u','o','i','b']
+print(sorted(List1))
+List2 = ['e','a','u','o','i','b']
+print(sorted(List2,reverse=True))
+
