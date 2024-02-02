a5cf3abda384f3c30ce5bf1c94c0851ffbcde338
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 21:17:23 +0600

    Assign values in Tuple

---
+++ Python Programming (28521)/8. Tuples Structure/8.3 Programming 1 Assign values in Tuple.py
@@ -0,0 +1,7 @@
+# Assign values in Tuple
+myinfo = ("Md. Delowar Hossan", "CSE","DUET",'https//www.facebook.com/delowar.designer/')
+Name, Dept, University, Contact = myinfo
+print(Name)
+print(University)
+print(Dept)
+print(Contact)
