dda247996856c2b6c4909f529a26b7bf88d9ec12
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 22:18:16 +0600

    Basic Tuple Concatenation operation

---
+++ Python Programming (28521)/8. Tuples Structure/8.5 Programming 1 Basic Tuple Concatenation operation.py
@@ -0,0 +1,8 @@
+# Basic Tuple Concatenation operation
+Tuple1 = ("Wahid",101325,"Delowar",602826)
+Tuple2 = ("Motu",984373,"Joty")
+Tuple3 = (89009,"Yafta",867409)
+Tuple4 = Tuple1 + Tuple2 + Tuple3
+print(Tuple4)
+Tuple5 = Tuple1 + ("My Name","Dep: CST",8887)
+print(Tuple5)
