ae05c99e91faa6739eca0678c2bca9807c1f37c1
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 09:21:03 +0600

    Built-in list functions and methods max functions

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 6 Built-in list functions and methods max functions.py
@@ -0,0 +1,6 @@
+# Built-in list functions and methods max functions
+# using max(arg1, * args)
+print('Maximum is:',max(1,3,2,5,47))
+# using max(iterable)
+num = [1,3,2,8,5,10,6,15]
+print('Maximum is:',max(num))
