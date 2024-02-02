ad396b16f72c810ba886f5a7a45e89e55843da2d
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 22:35:58 +0600

    Basic Tuple Membership operation in and not in

---
+++ Python Programming (28521)/8. Tuples Structure/8.5 Programming 3 Basic Tuple Membership operation.py
@@ -0,0 +1,13 @@
+# Basic Tuple Membership operation
+Tuple1 = ("Delowar",602826,"Motu",58975)
+tor = ("Joty",89709,"Majdur",90832)
+
+# Uses of  Membership operator in
+print("Fahim" in Tuple1)
+print("Delowar" in Tuple1)
+print("Majdur" in tor)
+print(44489709 in tor)
+
+# Uses of Membership operator not in
+print("Delowar" not in Tuple1)
+print("Delowar" not in tor)
