202b8eaf7cbdbba00029452672c0e72ff5e62d58
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 15:10:00 +0600

    List Methods Use in extend

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 12 List Methods Use in insert.py
@@ -0,0 +1,10 @@
+# List Methods Use in extend
+vowel = ['a','e','i','u']
+# inserting element to list at 4th position
+vowel.insert(3,'o')
+print('Updated List:',vowel)
+list1 = [{1,2},[5,6,7]]
+x = (3,4)
+# inserting x to the list
+list1.insert(1,x)
+print('Updated List:',list1)
