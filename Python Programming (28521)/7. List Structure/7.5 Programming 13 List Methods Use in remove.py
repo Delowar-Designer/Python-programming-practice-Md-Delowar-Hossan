e34b30cd84ae4877382d26a3ded8db531c5cd8a1
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 15:16:48 +0600

    List Methods Use in remove

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 13 List Methods Use in remove.py
@@ -0,0 +1,8 @@
+#  List Methods Use in remove
+vowel = ['a','e','i','o','u']
+vowel.remove('o')
+print('Updated List:',vowel)
+animal = ['cat','dog','dog','guinea pig','dog']
+animal.remove('guinea pig')
+print("Updated animal list:",animal)
+
