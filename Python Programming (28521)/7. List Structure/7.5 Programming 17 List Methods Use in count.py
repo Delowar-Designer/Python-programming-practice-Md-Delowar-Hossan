519c15acad2e11b02ea9a5352b1acc78c9981571
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 16:06:15 +0600

    List Methods Use in count

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 17 List Methods Use in count.py
@@ -0,0 +1,10 @@
+# List Methods Use in count
+vowels = ['a','e','i','o','i','u']
+# count element 'i'
+count = vowels.count('i')
+# print count
+print('The count of i is:', count)
+# count element'p'
+count = vowels.count('p')
+# print count
+print('The acunt of p is:',count)
