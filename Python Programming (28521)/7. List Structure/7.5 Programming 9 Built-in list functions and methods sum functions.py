3b70ccc10a80e21ec168222713b9822e92760434
Delowar-Designer <delowar.designer@gmail.com>
2024-02-02 09:38:46 +0600

    Built-in list functions and methods sum functions

---
+++ Python Programming (28521)/7. List Structure/7.5 Programming 9 Built-in list functions and methods sum functions.py
@@ -0,0 +1,8 @@
+# Built-in list functions and methods sum functions
+Numbers = [2.5,3,4,-5]
+# start parameter is not provided
+Summation = sum(Numbers)
+print(Summation)
+# start = 10
+Summation = sum(Numbers,10)
+print(Summation)
