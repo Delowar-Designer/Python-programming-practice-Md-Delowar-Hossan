ad4a9ad72b8196e17818c495941a4027eaef1873
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 10:41:31 +0600

    A Python program to find the maximum and minimum values of a set

---
+++ Python Programming (28521)/9. Set Structure/9.6 Programming 3 A Python program to find the maximum and minimum values of a set.py
@@ -0,0 +1,9 @@
+# A Python program to find the maximum and minimum values of a set
+setn = {5, 10, 3, 15, 2, 20}
+print("Original set elements:")
+print(setn)
+print(type(setn))
+print("\Maxium value of the said set:")
+print(max(setn))
+print("\nMinimum value of the said set:")
+print(min(setn))
