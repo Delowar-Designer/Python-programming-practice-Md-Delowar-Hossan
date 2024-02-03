8b8e0b227f50fb56e976046f3381e07d10857c88
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 09:53:11 +0600

    Deleting set elements

---
+++ Python Programming (28521)/9. Set Structure/9.4 Programming 3 Deleting set elements.py
@@ -0,0 +1,11 @@
+# Deleting set elements
+my_set = {1,3,4,5,6}
+my_set.pop()
+print(my_set)
+my_set.remove(6)
+print(my_set)
+my_set.discard(2)
+print(my_set)
+print(len(my_set))
+my_set.clear()
+print(my_set)
