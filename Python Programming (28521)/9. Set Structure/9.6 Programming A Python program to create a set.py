29dfab50ebbe220c3a3775ab22f81ded3d912532
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 10:21:47 +0600

    A Python program to create a set

---
+++ Python Programming (28521)/9. Set Structure/9.6 Programming A Python program to create a set.py
@@ -0,0 +1,13 @@
+# A Python program to create a set
+print("Create a new set:")
+x = set()
+print(x)
+print(type(x))
+print("\nCreate a non empty set:")
+n = set([0,1,2,3,4])
+print(n)
+print(type(n))
+print("\nUsing a Literal:")
+a = {1,2,3,'foo','Delowar'}
+print(type(a))
+print(a)
