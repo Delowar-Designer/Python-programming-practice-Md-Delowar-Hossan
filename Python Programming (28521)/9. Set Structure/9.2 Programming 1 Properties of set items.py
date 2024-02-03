0b7246f3a6052578026f42e6b47ff8c25295f5e6
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 09:11:20 +0600

    Properties of set items

---
+++ Python Programming (28521)/9. Set Structure/9.2 Programming 1 Properties of set items.py
@@ -0,0 +1,9 @@
+# Properties of set items
+a_set = {'a','b','c','d','c'}
+print(type(a_set))
+print(a_set)
+num_set = {1,2,3,4,5}
+word_set = set(["spam","eggs","bacon"])
+print(3 in num_set)
+print("spam" not in word_set)
+print(word_set)
