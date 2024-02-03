4bbe85102e95b98fc578d2e108e93d913e15485e
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 06:40:58 +0600

    Program to swipe the values of two tuples

---
+++ Python Programming (28521)/8. Tuples Structure/8.7 Progrtamming 3 Program to swipe the values of two tuples.py
@@ -0,0 +1,11 @@
+# Program to swipe the values of two tuples
+tuple1 = (10,20,30,40,50)
+tuple2 = ('Delowar','Moti','Joty','Yafta','Majdur')
+print("Before Swaping: ")
+print('Tuple1 =',tuple1)
+print('Tuple2 =',tuple2)
+tuple1,tuple2 = tuple,tuple1
+print("After Swaping: ")
+print('Tuple1 =',tuple1)
+print('Tuple2 =',tuple2)
+
