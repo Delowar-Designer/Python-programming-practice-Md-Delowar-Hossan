2c4437a3472d6f19781e6f7ec3671e5776dcdb71
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 09:25:46 +0600

    Creating a set using curly braces and set ()method

---
+++ Python Programming (28521)/9. Set Structure/9.3 Programming 1 Creating a set using curly braces and set ()method.py
@@ -0,0 +1,17 @@
+# Creating a set using curly braces and set ()method
+s = set("    ")
+print(type(s))
+print(s)
+
+Str_set = {'K','a','d','i','r'}
+print(type(Str_set))
+print(Str_set)
+
+num_set = {4,0,1,2,3}
+print(3 in num_set)
+print(num_set)
+
+word_set = set(["Kadir","Wahid","Meher","Sohel"])
+print(sorted(word_set))
+print("Kadir" not in word_set)
+print("Wahid" in word_set)
