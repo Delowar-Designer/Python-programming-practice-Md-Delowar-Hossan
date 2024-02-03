d7c2a3a1b5506cb0a5de7b12be15cfb4a33ec2ad
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 08:57:32 +0600

    Set Structure

---
+++ Python Programming (28521)/9. Set Structure/9.1 Programming 1 Set Structure.py
@@ -0,0 +1,10 @@
+# Set Structure
+a_set = {'a','b','c','d','c'}
+print(type(a_set))
+print(a_set)
+num_set = {1,2,3,4,5}
+word_set = set(["spam","eggs","bacon","Delowar Hossan"])
+print(3 in num_set)
+print("spam" not in word_set)
+print(word_set)
+
