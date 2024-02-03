a130c9ef79adea07c00513605a2889e217416fa9
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 06:29:45 +0600

    Programs for empty tuples, integer data, mixed data and nested and nested tuples

---
+++ Python Programming (28521)/8. Tuples Structure/8.7 Programming 2 Programs for empty tuples, integer data, mixed data and nested and nested tuples.py
@@ -0,0 +1,16 @@
+# Programs for empty tuples, integer data, mixed data and nested and nested tuples
+# empty tuple
+my_tuple = ()
+print(my_tuple)
+
+# Tuple having intergers
+my_tuple = (1,2,3)
+print(my_tuple)
+
+# Tuple with mixed data type.
+my_tuple = (1,"Helow Delowar",5.6)
+print(my_tuple)
+
+# Nested tuple
+my_tuple1 = ("Delowar",[8,4,6],(1,2,3))
+print(my_tuple1)
