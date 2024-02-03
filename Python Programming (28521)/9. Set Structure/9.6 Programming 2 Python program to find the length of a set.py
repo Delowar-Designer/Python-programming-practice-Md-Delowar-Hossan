dd1b2140313533740791bdece21a0d8e9ef6dc44
Delowar-Designer <delowar.designer@gmail.com>
2024-02-03 10:34:33 +0600

    Python program to find the length of a set

---
+++ Python Programming (28521)/9. Set Structure/9.6 Programming 2 Python program to find the length of a set.py
@@ -0,0 +1,19 @@
+# Python program to find the length of a set
+setn = {5, 10, 3, 15, 2, 20}
+print("Opiginal set elements:")
+print(setn)
+print(type(setn))
+print("\nLength of the said set:")
+print(len(setn))
+setn = {5,5,5,5,5,5}
+print("Opiginal set elements:")
+print(setn)
+print(type(setn))
+print("\nLength of the said set:")
+print(len(setn))
+setn = {5,5,5,5,5,5,7}
+print("Opiginal set elements:")
+print(setn)
+print(type(setn))
+print("\nLength of the said set:")
+print(len(setn))
