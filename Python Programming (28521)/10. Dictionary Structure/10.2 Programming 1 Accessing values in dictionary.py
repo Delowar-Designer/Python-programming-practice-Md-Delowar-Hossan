364a38b3445e3771ffa13c76f148de9fa36d7b5c
Delowar-Designer <delowar.designer@gmail.com>
2024-02-04 08:56:51 +0600

    Accessing values in dictionary

---
+++ Python Programming (28521)/10. Dictionary Structure/10.2 Programming 1 Accessing values in dictionary.py
@@ -0,0 +1,7 @@
+# Accessing values in dictionary
+mydict = {'Name':'Delowar','Roll':602826,'Department':'CSE','University':'DUET','Batch':'16th'}
+print(mydict['Name'])
+print(mydict['Roll'])
+print(mydict['Department'])
+print(mydict['University'])
+print(mydict['Batch'])
