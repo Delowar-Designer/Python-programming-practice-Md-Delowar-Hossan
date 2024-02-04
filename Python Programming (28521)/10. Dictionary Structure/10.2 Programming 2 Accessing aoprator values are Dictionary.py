b9fbdffdff27316b06125ab2436f88782bb68fce
Delowar-Designer <delowar.designer@gmail.com>
2024-02-04 09:03:54 +0600

    Accessing aoprator values are Dictionary

---
+++ Python Programming (28521)/10. Dictionary Structure/10.2 Programming 2 Accessing aoprator values are Dictionary.py
@@ -0,0 +1,5 @@
+# Accessing aoprator values are Dictionary
+mydict = {'Name:':'Delowar','Roll:':602826,'Department:':'CSE','University:':'DUET','Batch:':'16th'}
+print("Dictionary Values are")
+for value in mydict:
+    print(value,mydict[value])
