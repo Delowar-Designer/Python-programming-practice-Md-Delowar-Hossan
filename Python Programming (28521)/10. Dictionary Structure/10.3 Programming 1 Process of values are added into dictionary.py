05896005e9a3359ced5b7cc33090cfa0ef936447
Delowar-Designer <delowar.designer@gmail.com>
2024-02-04 10:45:11 +0600

    Process of values are added into dictionary

---
+++ Python Programming (28521)/10. Dictionary Structure/10.3 Programming 1 Process of values are added into dictionary.py
@@ -0,0 +1,9 @@
+# Process of values are added into dictionary
+mydict = {'Student1:':'Md. Delowar Hossan','Roll:': 602826}
+print("Before Assining value:")
+print(mydict)
+# Assign value
+mydict['University'] = 'DUET'
+mydict['Department'] = 'CSE'
+print("After Assining value: ")
+print(mydict)
