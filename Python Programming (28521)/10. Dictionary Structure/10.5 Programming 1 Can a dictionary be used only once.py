e0a6afce03b616e3ba4332c61871db9bdea730c4
Delowar-Designer <delowar.designer@gmail.com>
2024-02-05 14:46:55 +0600

    Can a dictionary be used only once?

---
+++ Python Programming (28521)/10. Dictionary Structure/10.5 Programming 1 Can a dictionary be used only once.py
@@ -0,0 +1,6 @@
+# Can a dictionary be used only once?
+dict = {'Name': 'Delowar', 'Age': 26, 'Name': 'Mitu'}
+print("dict['Name']:", dict['Name'])
+print(dict)
+print("dict['Age']:", dict['Age'])
+print(type(dict))
