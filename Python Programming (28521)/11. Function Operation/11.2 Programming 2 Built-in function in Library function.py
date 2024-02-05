e5bd08058040af6b9add809bbefca921387d1f09
Delowar-Designer <delowar.designer@gmail.com>
2024-02-05 20:49:50 +0600

    Built-in function in Library function

---
+++ Python Programming (28521)/11. Function Operation/11.2 Programming 2 Built-in function in Library function.py
@@ -0,0 +1,9 @@
+# Built-in function in Library function
+import math
+print(math.ceil(42.356))
+print(math.floor(42.356))
+print(math.sqrt(4))
+print(math.sin(45))
+print(math.cos(0))
+print(math.tan(45))
+print(math.radians(2))
