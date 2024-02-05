93dd53e4f023c3d3b1cf4189169b6a38293e9a3c
Delowar-Designer <delowar.designer@gmail.com>
2024-02-05 14:25:52 +0600

    Use the Clear method to remove the item with the specified key name

---
+++ Python Programming (28521)/10. Dictionary Structure/10.4 Programming 3 Use the Clear method to remove the item with the specified key name.py
@@ -0,0 +1,11 @@
+# Use the Clear method to remove the item with the specified key name
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+print("Befor using clear(): ")
+print(thisdict)
+thisdict.clear()
+print("After using clear(): ")
+print(thisdict)
