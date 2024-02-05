fe7241e31ad85b8218b09a9f9483fb5519cbf33b
Delowar-Designer <delowar.designer@gmail.com>
2024-02-05 21:16:08 +0600

    Calling a function

---
+++ Python Programming (28521)/11. Function Operation/11.3 Programming 1 Calling a function.py
@@ -0,0 +1,20 @@
+# Calling a function
+def mul(x,y):
+    return (x*y)
+print(mul(10,15))
+
+def div(A,B):
+    z=A/B
+    print(z)
+div(10,5)
+
+
+def welcame():
+    print("Welcame to Coimputer Technology")
+welcame()
+welcame()
+
+def square(T):
+    g = T * T
+    return g
+print(square(5) + square(5))
