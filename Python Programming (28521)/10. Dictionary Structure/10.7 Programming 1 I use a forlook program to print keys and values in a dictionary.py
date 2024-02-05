e4d00eefe813a7ad620045022f088da66b027cba
Delowar-Designer <delowar.designer@gmail.com>
2024-02-05 15:24:16 +0600

    I use a forlook program to print keys and values in a dictionary

---
+++ Python Programming (28521)/10. Dictionary Structure/10.7 Programming 1 I use a forlook program to print keys and values in a dictionary.py
@@ -0,0 +1,8 @@
+# I use a forlook program to print keys and values in a dictionary
+thisdict = {
+    "brand": "Delowar.Designer",
+    "model": "Md Delowar Hossan",
+    "year": 2022
+}
+for x in thisdict:
+    print(x,":",thisdict[x])
