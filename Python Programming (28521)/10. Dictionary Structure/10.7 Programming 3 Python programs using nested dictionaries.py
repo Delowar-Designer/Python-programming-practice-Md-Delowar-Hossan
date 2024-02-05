5c3ca6de4492210ab9b85d6a79d89194193c89b3
Delowar-Designer <delowar.designer@gmail.com>
2024-02-05 15:40:52 +0600

    Python programs using nested dictionaries

---
+++ Python Programming (28521)/10. Dictionary Structure/10.7 Programming 3 Python programs using nested dictionaries.py
@@ -0,0 +1,17 @@
+# Python programs using nested dictionaries
+myfamily = {
+    "child1" : {
+        "name": "Mysa",
+        "year": 2022
+    },
+    "child2" : {
+        "name": "Joty",
+        "year" : 2002
+    },
+    "child3" : {
+        "name" : "Delowar",
+        "year" : 2004
+    }
+}
+
+print(myfamily)
