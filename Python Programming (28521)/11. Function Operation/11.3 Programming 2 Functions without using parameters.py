f78cd90b4400e52ac4bfe4906560120b931d1132
Delowar-Designer <delowar.designer@gmail.com>
2024-02-05 21:26:32 +0600

    Functions without using parameters

---
+++ Python Programming (28521)/11. Function Operation/11.3 Programming 2 Functions without using parameters.py
@@ -0,0 +1,11 @@
+# Functions without using parameters
+def show():
+    print("Do not adopt unfair means in the exam")
+show()
+
+
+def add(a,b):
+    return(a+b)
+print(add(40,17))
+print(add(278,157))
+print(add(35,95))
