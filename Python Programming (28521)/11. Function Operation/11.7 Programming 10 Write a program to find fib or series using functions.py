099881317a7084b9cfa214b541ba0e0b6d700bed
Delowar-Designer <delowar.designer@gmail.com>
2024-02-06 15:20:17 +0600

    Write a program to find fib or series using functions

---
+++ Python Programming (28521)/11. Function Operation/11.7 Programming 10 Write a program to find fib or series using functions.py
@@ -0,0 +1,10 @@
+# Write a program to find fib or series using functions
+def fibo(n):
+    if n<=1:
+        return n
+    else:
+        return(fibo(n-1) + fibo(n-2))
+n = 10
+print("Fibo Series....: ")
+for i in range(n):
+    print(fibo(i),end=" ")
