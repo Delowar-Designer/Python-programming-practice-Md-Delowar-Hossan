1c5a54b01f188c792b8f4332a5a3dbcf7b6e87b1
Delowar-Designer <delowar.designer@gmail.com>
2024-02-06 10:55:23 +0600

    Date time module

---
+++ Python Programming (28521)/11. Function Operation/11.6 Programming 6 Date time module.py
@@ -0,0 +1,8 @@
+# Date time module
+import datetime
+n = datetime.datetime.now()
+t = datetime.datetime.today()
+u = datetime.datetime.utcnow()
+print("Now :",n)
+print("Today :",t)
+print("UTC now :",u)
