1becdb9f1dfc425dabadcf054b5759114ab3e65c
Delowar-Designer <delowar.designer@gmail.com>
2024-02-07 12:39:36 +0600

    Writing Python programming to find out someone's body condition as per specification

---
+++ Python Programming (28521)/12. Files input and Output Operation/12.5 Programming 3 Writing Python programming to find out someone's body condition as per specification.py
@@ -0,0 +1,10 @@
+# Writing Python programming to find out someone's body condition as per specification
+def program9():
+    f = open("info.txt","r")
+    print(f.tell()) #(i)
+    f.seek(4,0)     # (ii)
+    print(f.read(5)) # (iii)
+    f.seek(10,0)    # (iv)
+    print(f.tell())  # (V)
+    print(f.read(10)) # (vi)
+program9()
