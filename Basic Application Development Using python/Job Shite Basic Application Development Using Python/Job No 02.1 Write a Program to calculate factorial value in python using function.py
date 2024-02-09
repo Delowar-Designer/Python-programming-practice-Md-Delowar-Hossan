777e52616d605583d98609541a1c13814a2f4045
Delowar-Designer <delowar.designer@gmail.com>
2024-02-09 08:56:51 +0600

    Write a Program to calculate factorial value in python using function

---
+++ Basic Application Development Using python/Job Shite Basic Application Development Using Python/Job No 02.1 Write a Program to calculate factorial value in python using function.py
@@ -0,0 +1,9 @@
+# Write a Program to calculate factorial value in python using function
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return(x*factorial(x-1))
+num = int(input("Enter a number: "))
+result = factorial(num)
+print("The factorial of",num,"is",result)
