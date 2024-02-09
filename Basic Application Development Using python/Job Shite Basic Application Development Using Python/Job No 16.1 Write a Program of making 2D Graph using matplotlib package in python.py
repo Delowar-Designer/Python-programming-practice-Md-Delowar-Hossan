43fd1c557d7eb7485da3902e85d2546782380168
Delowar-Designer <delowar.designer@gmail.com>
2024-02-09 12:49:16 +0600

    Write a Program of making Bar plot using matplotlib in python

--- Basic Application Development Using python/Job Shite Basic Application Development Using Python/Job No 16.1 Write a Program of making 2D Graph using matplotlib package in python.py
+++ Basic Application Development Using python/Job Shite Basic Application Development Using Python/Job No 16.1 Write a Program of making 2D Graph using matplotlib package in python.py
@@ -1,9 +1,12 @@
 # Write a Program of making 2D Graph using matplotlib package in python
 from matplotlib import pyplot as plt
-x = [5,2,9,4,7]
-y = [10,5,8,4,2]
-plt.plot(x,y)
+
+x = [5, 2, 9, 4, 7]
+y = [10, 5, 8, 4, 2]
+
+plt.plot(x, y)
 plt.show()
+
 
 
 # Write a Program of making Bar plot using matplotlib in python
