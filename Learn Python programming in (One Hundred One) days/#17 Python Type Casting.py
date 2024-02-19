754ae13c2c612a242f3648749d25a5030381b99a
Delowar-Designer <delowar.designer@gmail.com>
2024-02-19 05:57:46 -0800

    Type Casting

---
+++ Learn Python programming in (One Hundred One) days/#17 Python Type Casting.py
@@ -0,0 +1,44 @@
+# Python Type Casting
+# int
+name = "122222"
+print(int(name))
+print(type(int(name)))
+
+x = int(1)
+y = int(2.22)
+z = int("32")
+print(x)
+print(type(int(x)))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+
+
+# Type Casting float
+U = float(1)
+S = float(2.8)
+T = float("3")
+R = float("4.5")
+print(U)
+print(type(U))
+print(S)
+print(type(S))
+print(T)
+print(type(T))
+print(R)
+print(type(R))
+
+
+# Tryit Casting in Str
+I = str("S1")
+J = str(222)
+K = str(34.5)
+print(I)
+print(type(I))
+print(J)
+print(type(J))
+print(K)
+print(type(K))
+
