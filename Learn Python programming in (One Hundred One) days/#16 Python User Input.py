d5c21a8de6badedf7c24d1d747168121d3fb28d7
Delowar-Designer <delowar.designer@gmail.com>
2024-02-19 05:30:53 -0800

    Python User Input

---
+++ Learn Python programming in (One Hundred One) days/#16 Python User Input.py
@@ -0,0 +1,13 @@
+#16 Python User Input
+Username = input("Enter your username:")
+PassWord = input("Enter your password:")
+
+print(Username)
+print(PassWord)
+
+
+username1 = input("Enter username:")
+print("Username is: " + username1)
+
+username2 = raw_input("Enter username:")
+print("Username is: " + username2)
