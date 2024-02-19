f6d72542a7e1af42472a18805c4d0f8110c8bcbc
Delowar-Designer <delowar.designer@gmail.com>
2024-02-19 04:54:25 -0800

    Python Swapping

--- Learn Python programming in (One Hundred One) days/#15 Python Swapping.py
+++ Learn Python programming in (One Hundred One) days/#15 Python Swapping.py
@@ -1,1 +1,13 @@
 #15 Python Swapping
+c = 50
+d = 60
+c,d = d,c
+print("This value is now c",c)
+print("This value is d = ",d)
+
+
+a = 'Delowar'
+b = 'Mitu'
+a,b = b,a
+print("This value Name is a",a)
+print("This value Name is b",b)
