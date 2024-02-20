64441b8252394adbc6dfa1b95a023afcd81c0dfc
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 06:51:10 -0800

    Python List Exercises

---
+++ Learn Python programming in (One Hundred One) days/#28 Python List Exercises.py
@@ -0,0 +1,10 @@
+#28 Python List Exercises
+# print the second item in the fruits list.
+fruit = ["apple", "banana", "cherry"]
+print(fruit[1])
+
+# change the value from "apple" to "Kiwi", in the fruits List.
+fruit = ["apple", "banana", "cherry"]
+print(fruit)
+fruit[0] = "kiwi"
+print(fruit)
