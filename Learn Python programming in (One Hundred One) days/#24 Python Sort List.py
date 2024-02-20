e3e09230dfc3f51fc40dc756022d9f6a13aa2248
Delowar-Designer <delowar.designer@gmail.com>
2024-02-19 20:29:31 -0800

    Python sort List

---
+++ Learn Python programming in (One Hundred One) days/#24 Python Sort List.py
@@ -0,0 +1,12 @@
+#24 Python Sort List
+number = [3,5,7,8,1,0,9,6]
+eng = ['a','r','t','y','u','d','w','g','h']
+number.sort()
+print(number)
+print(eng)
+
+# reverrse List
+number.sort(reverse = True)
+print(number)
+eng.sort(reverse = True)
+print(eng)
