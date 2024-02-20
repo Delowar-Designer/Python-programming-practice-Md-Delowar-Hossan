0ba90ed560a56c1aa3a7eb242fb3b5abb979216b
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 10:02:19 -0800

    Python Remove Set Items

---
+++ Learn Python programming in (One Hundred One) days/#40 Python Remove Set Items.py
@@ -0,0 +1,17 @@
+#40 Python Remove Set Items
+my_set = {1,2,3,4,5,6}
+my_set.remove(4)
+print(my_set)
+
+
+my_set2 = {"apple","banana","chang"}
+my_set2.discard("apple")
+print(my_set2)
+
+my_set3 = {"Delowar","Mitu","Joty"}
+my_set3.pop()
+print(my_set3)
+
+my_set4 = {"Delowar","Mitu","Joty"}
+my_set4.clear()
+print(my_set4)
