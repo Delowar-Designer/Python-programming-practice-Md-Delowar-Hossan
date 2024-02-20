26b4cc3d9dad13ac359e3e0652815d6d0ea2bd0d
Delowar-Designer <delowar.designer@gmail.com>
2024-02-19 18:10:54 -0800

    Python Access List Items

---
+++ Learn Python programming in (One Hundred One) days/#19 Python Access List Items.py
@@ -0,0 +1,8 @@
+#19 Python Access List Items
+Delowar = ['channelName', "Website", "Email", "Phone", "Group","Page"]
+Delowar[1]
+print(Delowar[3])
+
+# Chang list items
+Delowar[4] = "Linkedin Page"
+print(Delowar)
