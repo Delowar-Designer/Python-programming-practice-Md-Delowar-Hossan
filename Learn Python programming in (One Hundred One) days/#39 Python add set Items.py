33611c618e2aba546949d76de672d488d332855c
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 09:49:09 -0800

    Python add set Items

---
+++ Learn Python programming in (One Hundred One) days/#39 Python add set Items.py
@@ -0,0 +1,14 @@
+#39 Python add set Items
+my_set = {"Delowar","Mitu","Majdur","Yafta"}
+my_set.add("Joty")
+print(my_set)
+
+# set update()
+my_set2 = {"Delowar","Mitu","Majdur","Yafta"}
+set3 = (1,2,3,4)
+my_set2.update(set3)
+print("Update Set",my_set2)
+
+List2 = [40,50,60,70,80,90]
+my_set2.update(List2)
+print(my_set2)
