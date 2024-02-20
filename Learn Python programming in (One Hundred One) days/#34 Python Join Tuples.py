928c62fe73730af959b619ba8badc615d8362105
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 08:46:55 -0800

    Python Join Tuples

---
+++ Learn Python programming in (One Hundred One) days/#34 Python Join Tuples.py
@@ -0,0 +1,11 @@
+#34 Python Join Tuples
+num1 = (1,2,3,4,5,6,7,8,9,10,11)
+num2 = (12,13,14,15,16,17,18,19,20,21)
+num4 = ("Mitu","Joty","Majdur","Yafta","Delowar")
+num3 = num1 + num2
+
+print(num3)
+print("Multiply Tuples",num1 * 4)
+print(num4)
+print("Multiply Tuples in String")
+print(num4 * 3)
