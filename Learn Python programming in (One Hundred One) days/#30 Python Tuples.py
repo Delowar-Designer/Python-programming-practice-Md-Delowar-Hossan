5749d67b293c96fe5477720439d3cb9b57c985bd
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 07:22:49 -0800

    Python Tuples

---
+++ Learn Python programming in (One Hundred One) days/#30 Python Tuples.py
@@ -0,0 +1,15 @@
+#30 Python Tuples
+NewTuples = (1,2,3,"Delowar",False, )
+print(type(NewTuples))
+
+# NewTuples[1] = 20
+# print(NewTuples)
+
+# Negtive Indexing
+print(NewTuples[-2])
+
+# Renge of indexes
+thistuple =("apple","banana","cherry","orange","kiwi","melon","mango")
+print(thistuple[2:5])
+DDfrane = ("Deloware","Majudur","Joty","Mitu","Yafta")
+print(DDfrane[2:5])
