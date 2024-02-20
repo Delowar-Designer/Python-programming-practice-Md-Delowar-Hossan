b070df8f00767ac014107c26210ab21410871d65
Delowar-Designer <delowar.designer@gmail.com>
2024-02-19 17:45:58 -0800

    Python Lists

---
+++ Learn Python programming in (One Hundred One) days/#18 Python Lists.py
@@ -0,0 +1,11 @@
+#18 Python Lists
+li = [1,2,3,4,5,6,7]
+print(li)
+li [1] =10
+print(li)
+lis = ["eshan", "ahmed","ahad","Designer"]
+print(lis)
+list = ['Hello','Delowar','defe','time']
+print(list)
+print(type(list))
+
