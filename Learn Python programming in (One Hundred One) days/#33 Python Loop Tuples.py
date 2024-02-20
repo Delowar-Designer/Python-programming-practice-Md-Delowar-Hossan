f2deff557aaf921a486eaef9b12876894f1d967f
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 08:36:02 -0800

    Python Loop Tuples

---
+++ Learn Python programming in (One Hundred One) days/#33 Python Loop Tuples.py
@@ -0,0 +1,13 @@
+#33 Python Loop Tuples
+fruits = ['apple', 'banana', 'cherry', 'orange', 'dog']
+for i in fruits:
+    print(i)
+
+for x in range(len(fruits)):
+    print(fruits[x])
+
+myfrande = ("Delowar","Mitu","Joty","Majdur","Yafta")
+i = 1
+while i < len(myfrande):
+    print(myfrande[i])
+    i = i + 1
