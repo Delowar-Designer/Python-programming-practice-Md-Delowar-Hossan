e39474902faf3483697b871de34c31adf7b9dd23
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 09:00:29 -0800

    Python Tuple Method

---
+++ Learn Python programming in (One Hundred One) days/#35 Python Tuple Method.py
@@ -0,0 +1,6 @@
+#35 Python Tuple Method
+Tuples = ("Mitu","Joty","Majdur","Yafta","Delowar",602826,"Delowar","delowar","Delowar",)
+indexing = Tuples.index(602826)
+print("This Tuples indexing Numbdr",indexing)
+print('The Count Tuples')
+print(Tuples.count("delowar"))
