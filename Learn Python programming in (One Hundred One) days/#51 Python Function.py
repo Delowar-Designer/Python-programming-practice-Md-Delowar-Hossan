4b43c747e9c988f63d2f3771fe256be283b1535b
Delowar-Designer <delowar.designer@gmail.com>
2024-02-21 07:08:05 -0800

    Python Function

---
+++ Learn Python programming in (One Hundred One) days/#51 Python Function.py
@@ -0,0 +1,22 @@
+#51 Python Function
+sum = 20
+sum1 = 40
+sum3 = sum + sum1
+print(sum3)
+sum4 = 14
+sum5 = 45
+sum6 = sum4 - sum5
+print(sum6)
+
+def Delowar(a, b):
+    Sum = a + b
+    print(Sum)
+
+def Minus(c, d):
+    mins = c - d
+    print(mins)
+
+Delowar(70, 43)
+Minus(14, 7)
+
+
