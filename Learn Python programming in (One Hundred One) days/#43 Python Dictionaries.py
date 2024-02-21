c687b1f087e9905286461fca3d31fc0354f7f927
Delowar-Designer <delowar.designer@gmail.com>
2024-02-20 22:43:39 -0800

    Python Dictionaries

---
+++ Learn Python programming in (One Hundred One) days/#43 Python Dictionaries.py
@@ -0,0 +1,20 @@
+#43 Python Dictionaries
+StudentInfo = {
+    "Delowar": {
+        "First Name": "Md Delowar Hossan",
+        "study": "12",
+        "Subject": "Computer Science",
+        "Roll" : 602826,
+        "Number" : 1773873289
+    },
+"Mitu": {
+        "First Name": "Md Mitu Hossan",
+        "study": "13",
+        "Subject": "Science",
+        "Roll" : 602866,
+        "Number" : 13873289
+}
+}
+print(StudentInfo)
+print(StudentInfo["Mitu"])
+print(StudentInfo["Delowar"]["Roll"])
