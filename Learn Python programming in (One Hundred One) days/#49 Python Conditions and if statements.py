15c97b6b683ac9876a5dfec0d5e42062f0084e00
Delowar-Designer <delowar.designer@gmail.com>
2024-02-21 02:18:20 -0800

    Python Conditions and if statements

---
+++ Learn Python programming in (One Hundred One) days/#49 Python Conditions and if statements.py
@@ -0,0 +1,13 @@
+#49 Python Conditions and if statements
+a = 100
+b = 80
+if a > b:
+    print("ami b er theke boro")
+elif a < b:
+    print("b a er theke boro")
+elif a != b:
+    print("Kino tay thik na")
+elif a == b:
+    print("2 tay soman")
+else:
+    print("a ber theke boro na")
