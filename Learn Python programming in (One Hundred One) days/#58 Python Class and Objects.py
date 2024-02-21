f0d8ddf4dd729eb92f6fd6e127612b34dd0c285f
Delowar-Designer <delowar.designer@gmail.com>
2024-02-21 08:52:37 -0800

    Python Class and Objects

---
+++ Learn Python programming in (One Hundred One) days/#58 Python Class and Objects.py
@@ -0,0 +1,17 @@
+#58 Python Class and Objects
+class Student:
+    name = ""
+    number = ""
+    roll = ""
+    study = ""
+    gf = ""
+
+a = Student()
+b = Student()
+
+a.name = "Delowar"
+a.number = "12"
+b.name = "Hossan"
+print(a.name)
+print(a.number)
+print(b.name)
