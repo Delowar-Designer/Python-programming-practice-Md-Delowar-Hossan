b18da19e0616a0eb91dc8f0b4012d3fdc3d595e8
Delowar-Designer <delowar.designer@gmail.com>
2024-02-21 09:05:03 -0800

    Pythonn Inheritance

---
+++ Learn Python programming in (One Hundred One) days/#59 Python Inheritance.py
@@ -0,0 +1,13 @@
+#59 Pythonn Inheritance
+class Person:
+    car = "BMW"
+    tk = "100 Crore"
+    Home = "1l Floor"
+
+class Person2(Person):
+    Mobil = ""
+    BrokenHome = ""
+
+k = Person2()
+print(k.tk)
+
