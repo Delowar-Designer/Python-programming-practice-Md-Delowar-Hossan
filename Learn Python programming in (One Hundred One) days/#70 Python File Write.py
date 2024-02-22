66d1253124bb1ae1a7d7bf0c32f834a85d119851
Delowar-Designer <delowar.designer@gmail.com>
2024-02-22 02:50:10 -0800

    Python File Write

---
+++ Learn Python programming in (One Hundred One) days/#70 Python File Write.py
@@ -0,0 +1,10 @@
+#70 Python File Write
+# Wr = open("delowar2.html","a")
+# Wr.write("<p>My Name is Md Delowar Hossan</p>\n")
+# Wr.write("<h>I am Software Engineering</h>\n")
+# Wr.write("<h>Aslamualai Hello there </h>\n")
+# Wr.write("My Skills Languages Tools\n")
+wr = open("text.txt","r")
+
+print(wr.read())
+
