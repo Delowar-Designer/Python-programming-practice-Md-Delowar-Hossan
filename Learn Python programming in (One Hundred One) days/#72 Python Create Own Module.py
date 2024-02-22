721f9ae1206bc4fc13afed5ca8d04258fcd41d96
Delowar-Designer <delowar.designer@gmail.com>
2024-02-22 05:06:46 -0800

    Python Create Own Module

---
+++ Learn Python programming in (One Hundred One) days/#72 Python Create Own Module.py
@@ -0,0 +1,12 @@
+#72 Python Create Own Module
+
+import Module as you
+
+you.Delowar()
+you.Mitu()
+you.Joty()
+print(you.person1)
+print(you.person1["age"])
+
+from Module import Mitu
+Mitu()
---
+++ Learn Python programming in (One Hundred One) days/Module.py
@@ -0,0 +1,16 @@
+def Delowar():
+    print("Delowar is now An bad Module")
+
+def Mitu():
+    print("Mitu is now An bad Module")
+
+def Joty():
+    print("Joty is now An bad Module")
+
+list = ['Delowar','Mitu','Joty','Yafta',"Majdur"]
+
+person1 = {
+    "name":"Joty",
+    "age": 26,
+    "country": "Bangladesh"
+}
