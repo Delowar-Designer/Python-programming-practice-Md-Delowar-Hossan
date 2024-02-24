09ea04efe2a6c561185c7c9f68e25cc3035deeae
Delowar-Designer <delowar.designer@gmail.com>
2024-02-24 01:35:44 -0800

    Python Project 2 Mad Libs Game

---
+++ Learn Python programming in (One Hundred One) days/# Python Project 2 Mad Libs Game.py
@@ -0,0 +1,9 @@
+# Python Project 2 Mad Libs Game
+username = input("What is your name: ")
+fatherName = input("What is your father name: ")
+matherName = input("What is your mother name: ")
+study = input("which class do you read in: ")
+relationship = input("tell me about your relationship status: ")
+
+print(f"My Doggy Name is {username}, My doggvs father name {fatherName}, my doggys mather name is {matherName}"
+      f"my do doggy study in class {study}, our relationship is {relationship}")
