655d898b1e4e55f10ea5dea8a73cdf16852cb226
Delowar-Designer <delowar.designer@gmail.com>
2024-02-24 07:16:34 -0800

    Python Project 8 Desktop Notifier App

---
+++ Learn Python programming in (One Hundred One) days/# Python Project 8 Desktop Notifier App.py
@@ -0,0 +1,8 @@
+# Python Project 8 Desktop Notifier App
+from win10toast import ToastNotifier
+Toast = ToastNotifier()
+
+Toast.show_toast("Learning Time", "Hello Delowar It's time To learn Something New", duration=10)
+Toast.show_toast("Learning Time", "Hello Delowar It's time To learn Something New", duration=5)
+Toast.show_toast("Learning Time", "Hello Delowar It's time To learn Something New", duration=2)
+Toast.show_toast("Learning Time", "Hello Delowar It's time To learn Something New", duration=1)
