0966bd395dacb1b95c9e7ed4146705f3896caa39
Delowar-Designer <delowar.designer@gmail.com>
2024-02-27 21:03:59 -0800

    Data Types

---
+++ Python Programing Praktice Zulkarnine Mahmud/6 Data Types.py
@@ -0,0 +1,30 @@
+# Data Types
+number_value = 32
+second_value = 4
+addition = number_value + second_value
+subtraction = number_value - second_value
+multiplication = number_value * second_value
+division = number_value / second_value
+mul = number_value * 2
+div = number_value / 5
+div_int = number_value // 2
+remainder = number_value % 5
+
+print(f"Addtion: {addition}")
+print(f"sub: {subtraction}")
+print(f"multiplication: {multiplication}")
+print(f"division: {division}")
+print(f"mul: {mul}")
+print(f"div: {div}")
+print(f"div_int: {div_int}")
+print(f"remainder: {remainder}")
+
+person_name = "Delowar"
+food = " Bloger"
+value2 = 602826
+
+concatented_string = person_name + food
+print(concatented_string)
+Delowar_with_value = person_name +" "+ str(value2)
+print(Delowar_with_value)
+print(f"Length of name: {len(Delowar_with_value)}")
