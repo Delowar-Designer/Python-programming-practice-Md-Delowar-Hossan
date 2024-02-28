aaedf322d30c32d48c281908aeae59562f0d8566
Delowar-Designer <delowar.designer@gmail.com>
2024-02-27 21:01:04 -0800

    Complicated Funceon

---
+++ Python Programing Praktice Zulkarnine Mahmud/4 Complicated Funceon.py
@@ -0,0 +1,10 @@
+#4 Complicated Funceon
+def complicated_logic(first, second):
+    return first + second * 12 - 4 * 12
+
+number_1 = 10
+number_2 = 5
+sum = complicated_logic(number_1, number_2)
+
+print(f"{number_1} + {number_2} = {number_1 + number_2}")
+print(f"{number_1} + {number_2} = {sum}")
