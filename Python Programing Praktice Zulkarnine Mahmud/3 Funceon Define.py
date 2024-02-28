5fa182f46515d161e0eab9b20b69cd5c26ad2371
Delowar-Designer <delowar.designer@gmail.com>
2024-02-27 20:11:09 -0800

    FFunceon Define

---
+++ Python Programing Praktice Zulkarnine Mahmud/3 Funceon Define.py
@@ -0,0 +1,9 @@
+def add_two_values(first, second):
+    return first + second
+
+number_1 = 10
+number_2 = 52
+sum = add_two_values(number_1, number_2)
+
+print(f"{number_1} + {number_2} = {number_1 + number_2}")
+print(f"{number_1} + {number_2} = {sum}")
