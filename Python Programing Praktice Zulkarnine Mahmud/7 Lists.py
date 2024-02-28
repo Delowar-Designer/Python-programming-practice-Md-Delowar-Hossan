adccba9288f5810ca30974eea99b51799f7f677a
Delowar-Designer <delowar.designer@gmail.com>
2024-02-27 23:56:11 -0800

    Lists

---
+++ Python Programing Praktice Zulkarnine Mahmud/7 Lists.py
@@ -0,0 +1,14 @@
+# Lists
+grocery_list = ["apple", "banana", "Banana"]
+l2 = list()
+second_item = grocery_list[0]
+print(second_item)
+
+print(grocery_list)
+grocery_list.append("paineappel")
+print(grocery_list)
+print(l2)
+l2.append(4)
+print(l2)
+l2.append("computer")
+print(l2)
