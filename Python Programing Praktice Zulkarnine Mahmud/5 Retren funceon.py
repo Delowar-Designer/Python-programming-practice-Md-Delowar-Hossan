43c3d2b5a5b193f0f35719b875cb327268873962
Delowar-Designer <delowar.designer@gmail.com>
2024-02-27 21:02:49 -0800

    Retren Funceon

---
+++ Python Programing Praktice Zulkarnine Mahmud/5 Retren funceon.py
@@ -0,0 +1,10 @@
+print("1")
+def complcated_logic(first, second):
+    print(f"You Passed: {first} {second}")
+
+number_1 = 10
+number_2 = 70
+print("2")
+complcated_logic(number_1, number_2)
+complcated_logic(3,5)
+print("3")
