2fc3b5a01a1b460a46e2edae4b64622039484428
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 08:01:16 -0800

    Os Module

---
+++ Python Progrmming Praktice StartBit Delowar/19 OS Module.py
@@ -0,0 +1,18 @@
+# OS Module
+#import os
+#print(os.getcwd())
+#os.mkdir("NewFolder")
+#os.rename('NewFolder','Delowar')
+#os.rmdir("Delowar")
+#print(dir(os))
+#print(os.path.isfile("Wrriting to File.py"))
+'''fileloc = 'OS Module.py'
+if os.path.isfile(fileloc):
+    print("File", fileloc, "exists")
+else:
+    print("File", fileloc, "don\'t exists")'''
+
+#print(os.path.isdir('dir'))
+
+import os as Operatoring
+print(Operatoring.getcwd())
