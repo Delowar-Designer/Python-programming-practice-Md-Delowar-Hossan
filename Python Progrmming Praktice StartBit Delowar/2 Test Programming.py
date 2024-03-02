af1959bbaa4e162c97be88546914bd8beed00e30
Delowar-Designer <delowar.designer@gmail.com>
2024-03-01 19:48:04 -0800

    Test Programming

---
+++ Python Progrmming Praktice StartBit Delowar/2 Test Programming.py
@@ -0,0 +1,11 @@
+# 2 Test Programming
+print('Hello Delowar Hossan')
+
+veriabole = 5
+veriabole1 = 20
+veriabole2 = veriabole + veriabole1
+
+print(veriabole2)
+
+print('Hello I\'m Delowar Hossan')
+print("Hello I\'m Delowar Hossan")
