fd2ad06bbe29482012477827beb316beef247412
Delowar-Designer <delowar.designer@gmail.com>
2024-03-01 20:37:54 -0800

    iff else statements

---
+++ Python Progrmming Praktice StartBit Delowar/4 if else statements.py
@@ -0,0 +1,14 @@
+# If Else Statments
+# if(condition):
+#     task
+# else:
+#     task
+
+age = 8
+
+if(age >= 13):
+    print("you are a kid")
+elif(age <= 18):
+    print("you ara a teen ager")
+else:
+    print("you are not an adult")
