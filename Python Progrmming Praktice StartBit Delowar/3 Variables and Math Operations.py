b02b6e21eb00e8738179949428f563be3ba5c835
Delowar-Designer <delowar.designer@gmail.com>
2024-03-01 20:12:49 -0800

    3 Variables and Math Operations

---
+++ Python Progrmming Praktice StartBit Delowar/3 Variables and Math Operations.py
@@ -0,0 +1,13 @@
+# 3 Variables and Math Operations
+myVer = "I love Bangladesh"
+print(myVer)
+
+text = "I love Bangladesh "
+math = 2 + 2
+result = str(math)
+print(text + result)
+
+x,y,z = (4,5,6)
+print(x)
+print(y)
+print(z)
