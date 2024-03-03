e1fea7b28872118a6f04da7de26080fd62743c99
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 22:08:43 -0800

    for Loop and range function in Python

---
+++ Python Progrmming Darun It Praktice Delowar/18 for Loop and range function in Python.py
@@ -0,0 +1,9 @@
+# for Loop and range function in Python
+friends = ["Delowar","Mitu","Joty","James","James"]
+for friend in friends:
+    print(friend)
+    print("Hello Friend")
+
+for x in range(1,101):
+    print(x)
+
