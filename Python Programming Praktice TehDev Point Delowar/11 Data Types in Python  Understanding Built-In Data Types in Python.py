ae3631ad5a85918c4724733ee2adcbaf0d0f3cef
Delowar-Designer <delowar.designer@gmail.com>
2024-03-03 06:21:12 -0800

    Data Types in Python | Understanding Built-In Data Types in Python

---
+++ Python Programming Praktice TehDev Point Delowar/11 Data Types in Python  Understanding Built-In Data Types in Python.py
@@ -0,0 +1,33 @@
+# Data Types in Python | Understanding Built-In Data Types in Python
+a = 12
+print(a)
+print(type(a))
+
+a = 12.5
+print(a)
+print(type(a))
+
+a = 12 + 2j
+print(a)
+print(type(a))
+
+x = 'Hello'
+y = 'world'
+z = '''
+Hello
+World
+python
+'''
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+l = ['a', 10, True,'Delowar']
+print(1)
+print(type(1))
+l[0] = 'Orange'
+print(l)
+print(type(l))
+
