e8de13cffb1c822cc4d794df88b8abf3876c7353
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 20:10:39 -0800

    Collecting and Working With User Input in Python

---
+++ Python Progrmming Darun It Praktice Delowar/10 Collecting and Working With User Input in Python.py
@@ -0,0 +1,11 @@
+# Collecting and Working With User Input in Python
+name = input("What is your name? ")
+age = input("How old are your? ")
+roll = input("Your Roll? ")
+
+print(f"Student name is {name}. Student age is {age}. Student roll is {roll}.")
+
+number_one = input("Enter number one ")
+number_tow = input("Enter number tow ")
+
+print(f"Sum is {number_one + number_tow}")
