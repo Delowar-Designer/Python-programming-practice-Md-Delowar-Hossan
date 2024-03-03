63b04a9db468af4836073dc840f3b07c77e239d8
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 18:07:45 -0800

    Print Python Program

---
+++ Python Progrmming Darun It Praktice Delowar/1 Print Python Program.py
@@ -0,0 +1,9 @@
+# Print python Program
+print('Programer Delowar.Designer')
+first_name = "Delowar"
+last_name = "Hossan"
+print(first_name +" "+ last_name)
+number = 12
+Number = 25
+print(number)
+print(Number)
