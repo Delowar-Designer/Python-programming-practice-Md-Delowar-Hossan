eb6fd95e72faf03fc5cb1504517a5361b643b22d
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 21:58:57 -0800

    Nested If with Example Problem in Python

---
+++ Python Progrmming Darun It Praktice Delowar/17 Nested If with Example Problem in Python.py
@@ -0,0 +1,9 @@
+# Nested If with Example Problem in Python
+number = int(input("Enter your number: "))
+if number >= 0:
+    if number == 0:
+        print("Number is Zero")
+    else:
+        print("positive Number")
+else:
+    print("Negative Number")
