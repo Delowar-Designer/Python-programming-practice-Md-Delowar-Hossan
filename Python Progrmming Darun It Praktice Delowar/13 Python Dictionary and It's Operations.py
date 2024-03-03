c38699046c1d4f8a25c2c7a7b97d0dcd7144a563
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 20:50:28 -0800

    Python Dictionary and It's Operations

---
+++ Python Progrmming Darun It Praktice Delowar/13 Python Dictionary and It's Operations.py
@@ -0,0 +1,11 @@
+# Python Dictionary and It's Operations
+students = {"Delowar":18, "Mitu":19, "Joty":23}
+
+print(students["Mitu"])
+print(students.keys())
+print(students.values())
+print(students.items())
+students.update({"Delowar": 19, "Yafta":20})
+print(students)
+print(students.get("Kabir"))
+
