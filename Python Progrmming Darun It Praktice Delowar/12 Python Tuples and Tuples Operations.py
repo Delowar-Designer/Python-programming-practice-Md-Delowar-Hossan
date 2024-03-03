bd98757e3b179d03cc74821221949f5c0eaf4249
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 20:39:35 -0800

    ython Tuples and Tuples Operations

---
+++ Python Progrmming Darun It Praktice Delowar/12 Python Tuples and Tuples Operations.py
@@ -0,0 +1,7 @@
+# Python Tuples and Tuples Operations
+person_one = ("Delowar","Delowar", 25, "Dinajpr","Bangladesh")
+
+print(type(person_one))
+print(person_one[3])
+print(person_one.count("Delowar"))
+print(person_one.index("Dinajpr"))
