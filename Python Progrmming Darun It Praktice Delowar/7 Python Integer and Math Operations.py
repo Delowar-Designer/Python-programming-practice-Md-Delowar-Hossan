fd362219fdd0e58bad0ba610025fd5b7b052f4db
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 19:06:27 -0800

    Python Integer and Math Operations

---
+++ Python Progrmming Darun It Praktice Delowar/7 Python Integer and Math Operations.py
@@ -0,0 +1,18 @@
+# Python Integer and Math Operations
+print(8+3) # Addition +
+print(99-3) # Subtraction -
+print(33*8) # Multiplication *
+print(33/2) # Division /
+print(23//3) # Floor Division //
+print((10**2)) # Exponent **
+print(10%3) # Modulus %
+      # Increment
+nuumber = 12
+nuumber += 1
+print(nuumber)
+     # Decrement
+nuumber = 12
+nuumber -= 1
+print(nuumber)
+
+print(23.3 +22.3)
