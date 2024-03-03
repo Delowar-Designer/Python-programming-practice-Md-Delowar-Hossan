e9e1dee5579d45fa9ed252babded3015c1b2502e
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 21:47:32 -0800

    Find the Largest Among Three Numbers

---
+++ Python Progrmming Darun It Praktice Delowar/16 Find the Largest Among Three Numbers.py
@@ -0,0 +1,11 @@
+# Find the Largest Among Three Numbers
+a = 100
+b = 38
+c = 399
+
+if a>b and a>c:
+    print("a is the largest number")
+elif b>a and b>c:
+    print("b is the largest number")
+else:
+    print("C is the largest number")
