d01e909b6ac7fe7d9709b222694466193861e360
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 18:23:49 -0800

    Python Data Types in Bangla

---
+++ Python Progrmming Darun It Praktice Delowar/6 Python Data Types in Bangla.py
@@ -0,0 +1,16 @@
+# Python Data Types in Bangla
+one = "Delowar"
+two = 189498
+three = 929.33
+four = ("Mitu",19992,"Dinajpu")
+five = ["Delowar", "Mitu",30,9093,"Joty"]
+six = {"one":2,"two":4,'three':3,"ood": 22}
+seven = {1,2,3,4}
+
+print(type(one))
+print(type(two))
+print(type(three))
+print(type(four))
+print(type(five))
+print(type(six))
+print(type(seven))
