9764c53db36c1f8de261a5f36f352a817a5e5fc5
Delowar-Designer <delowar.designer@gmail.com>
2024-03-02 19:44:01 -0800

    Python String and String Methods

---
+++ Python Progrmming Darun It Praktice Delowar/8 Python String and String Methods.py
@@ -0,0 +1,14 @@
+# Python String and String Methods
+name = "I love Python Programming language. It's a powerful programming language."
+print(name)
+print(name[0:35])
+
+f_name = "Delowar"
+l_name = "Hossan"
+print(f_name +" "+ l_name)
+print(len(f_name))
+print(f_name.upper())
+print(f_name.lower())
+print(f_name.title())
+print(f_name.replace("D","Mitu D"))
+print(f_name.islower())
