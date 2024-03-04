ab9074a86c5b99aa1d47f67886a2e1c3ebb33e0c
Delowar-Designer <delowar.designer@gmail.com>
2024-03-04 00:57:21 -0800

    Python Pattern Designe 5 Invrted Mirrored Right Triangle

---
+++ Codinglaugh Marjuk Ahmed Diddiki Praktice Delowar Hossan/All Pattern Designe in Python Programming/Python Pattern Designe 5 Invrted Mirrored Right Triangle.py
@@ -0,0 +1,10 @@
+# Python Pattern Designe 5 Invrted Mirrored Right Triangle
+a = int(input("Enter a value: "))
+
+for i in range(a,0,-1):
+    s = i-1
+    for j in range(0,i):
+        print("#",end="")
+    print()
+    for k in range(0,a-s):
+        print(" ",end="")
