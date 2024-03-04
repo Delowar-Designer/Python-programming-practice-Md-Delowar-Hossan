e51166a7552fce3b7cd81e1c70b3bb7db08458ee
Delowar-Designer <delowar.designer@gmail.com>
2024-03-03 22:13:07 -0800

    Python Pattern Designe 1 Pattern Introduction, Star, Alphabetic

---
+++ Codinglaugh Marjuk Ahmed Diddiki Praktice Delowar Hossan/All Pattern Designe in Python Programming/Python Pattern Designe 1 Pattern Introduction, Star, Alphabetic.py
@@ -0,0 +1,16 @@
+# Python Pattern Designe 1 Pattern Introduction, Star, Alphabetic
+'''a = input ()
+b = input()
+print(a+b)'''
+
+'''c = int(input("Enter the first value: "))
+d = int(input("Enter the second value: "))
+print(c+d)
+print("Summation: ",c+d,'\n codeder Delowar')
+'''
+#help(print)
+
+# print("code",end =' ')
+# print("Delowar")
+for i in range(1,11):
+    print(i)
