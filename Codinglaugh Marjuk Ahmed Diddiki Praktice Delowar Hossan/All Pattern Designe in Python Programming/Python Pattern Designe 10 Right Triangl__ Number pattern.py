1f4a315d62a53e0dcb774788ec4230e320a99e49
Delowar-Designer <delowar.designer@gmail.com>
2024-03-05 00:42:02 -0800

    Python Pattern Designe 10 Right Triangl__ Number pattern

---
+++ Codinglaugh Marjuk Ahmed Diddiki Praktice Delowar Hossan/All Pattern Designe in Python Programming/Python Pattern Designe 10 Right Triangl__ Number pattern.py
@@ -0,0 +1,8 @@
+# Python Pattern Designe 10 Right Triangl__ Number pattern
+a = int(input("Enter a state number: "))
+#b = int(input("Enter Value: "))
+
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print(j," ",end="")
+    print()
