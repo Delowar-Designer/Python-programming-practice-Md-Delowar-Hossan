3b6d2e8041d3a60244ca3164e5ef7b99a793dfc2
Delowar-Designer <delowar.designer@gmail.com>
2024-03-10 21:20:50 -0700

    Python Pattern Designe 52 Love Pattern Python Progrram

--- Codinglaugh Marjuk Ahmed Diddiki Praktice Delowar Hossan/All Pattern Designe in Python Programming/Python Pattern Designe 52 Love Pattern Python Program.py
+++ Codinglaugh Marjuk Ahmed Diddiki Praktice Delowar Hossan/All Pattern Designe in Python Programming/Python Pattern Designe 52 Love Pattern Python Program.py
@@ -1,2 +1,9 @@
-ssgsggssgsg
-kljslk
+# Python Pattern Designe 52 Love Pattern Python Progrram
+x = input("Enter Pattern Print View value Boder: ")
+for row in range(0,6):
+    for col in range(0,7):
+        if((row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8)):
+            print(x," ",end="")
+        else:
+            print("   ",end="")
+    print()
