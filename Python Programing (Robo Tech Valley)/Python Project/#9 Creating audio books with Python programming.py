04424d767448967feb3a4c523c46f6f204e118b3
Delowar-Designer <delowar.designer@gmail.com>
2024-03-26 02:02:53 -0700

    reating audio books with Python programming

---
+++ Python Programing (Robo Tech Valley)/Python Project/#9 Creating audio books with Python programming.py
@@ -0,0 +1,12 @@
+# Creating audio books with Python programming
+from gtts import gTTS
+import os
+
+file = open("TextFile.txt", "r")
+myText = file.read().replace("\n", " ")
+language = "bn"
+
+output = gTTS(text = myText, lang = language, slow = False)
+output.save("output.mp3")
+file.close()
+os.system("start output.mp3")
