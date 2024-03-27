a13970320d093b6d99eaa1195905279a046566fc
Delowar-Designer <delowar.designer@gmail.com>
2024-03-27 06:21:28 -0700

    Python Variables Data Types Type Casting

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#3 Python Variables Data types Type Casting.py
@@ -0,0 +1,41 @@
+x = "Delowar"
+
+def MyFunc():
+    print("Python is " + x)
+
+MyFunc()
+
+#Python Variables
+x = 5
+y = "Mitu"
+print(x)
+print(y)
+
+
+# Built in Data Types
+x = 1 # int
+y = 2.9 # Float
+z = 1j # Complex
+type(x)
+type(y)
+type(z)
+
+x = "awesome string" # Global variable
+def myfunc():
+    name = "Mitu"
+    print("Python is " + x)
+    print(name)
+myfunc()
+
+print(type(x))
+
+#
+x = float(12)
+y = int(1.3)
+z = "Delowar"
+print(type(x))
+print(type(y))
+print(type(z))
+print(x)
+print(y)
+print(z)
