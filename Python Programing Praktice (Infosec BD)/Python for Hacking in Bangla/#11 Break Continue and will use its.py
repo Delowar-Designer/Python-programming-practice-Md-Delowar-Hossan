bdfc33b99887081e2ca7789ceed2e7ddcbf0cd7f
Delowar-Designer <delowar.designer@gmail.com>
2024-03-27 09:29:04 -0700

    Break Continue and will use its

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#11 Break Continue and will use its.py
@@ -0,0 +1,14 @@
+# Break Continue and will use its
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+    if (i == 4):
+        break
+
+i = 1
+while i < 6:
+    i += 1
+    if (i == 4):
+        continue
+    print(i)
