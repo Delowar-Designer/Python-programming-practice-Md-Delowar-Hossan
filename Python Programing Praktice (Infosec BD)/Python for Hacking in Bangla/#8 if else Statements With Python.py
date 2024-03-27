95784e2189ee080554f9850ba9765a37a3a15d53
Delowar-Designer <delowar.designer@gmail.com>
2024-03-27 08:12:59 -0700

    if else Statements With Python

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#8 if else Statements With Python.py
@@ -0,0 +1,17 @@
+# if else Statements With Python
+a = 100
+b = 20
+if a > b:
+    print("a is big")
+else:
+    print("b is big")
+
+naumber1 = int(input("Enter value of Number1: "))
+naumber2 = int(input("Enter value of Number2: "))
+if naumber1 > naumber2:
+    print("naumber1 is big")
+
+if naumber1 == naumber2:
+    print("naumber1 and number2 is equal")
+else:
+    print("naumber2 is big")
