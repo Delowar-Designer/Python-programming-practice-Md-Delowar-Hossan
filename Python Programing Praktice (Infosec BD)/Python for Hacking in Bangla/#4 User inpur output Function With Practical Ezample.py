eb5eb2f92a2bb043baa924cd9639697d8cd91a64
Delowar-Designer <delowar.designer@gmail.com>
2024-03-27 06:41:40 -0700

    User inpur output Function With Practical Ezample

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#4 User inpur output Function With Practical Ezample.py
@@ -0,0 +1,18 @@
+# User inpur output Function With Practical Ezample
+'''data = input("Enter your value:")
+data2 = input()
+
+data3 = int(input())
+'''
+
+data = 12
+x = '23'
+print(data +int(x))
+print("The value of data is : ",data)
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name)
+print(age)
+print("My name is ",name)
+print("My age is ",age)
