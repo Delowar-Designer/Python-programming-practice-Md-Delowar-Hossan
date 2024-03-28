90a4b91999aebb48164b0e0bf5af6353234fca4c
Delowar-Designer <delowar.designer@gmail.com>
2024-03-28 11:02:13 -0700

    Send Unlimited SMS to Friend Message Booming in python

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#23 Send Unlimited SMS to Friend Message Booming in python.py
@@ -0,0 +1,9 @@
+#23 Send Unlimited SMS to Friend Message Booming in python
+import pyautogui as p
+import time
+
+time.sleep(4)
+
+while True:
+    p.typewrite("I am Delowar Hossan, Your are Fine")
+    p.press("enter")
