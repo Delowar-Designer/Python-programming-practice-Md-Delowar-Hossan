ccb4937a89bc3556de5000bc17f7da1c4ea2a58b
Delowar-Designer <delowar.designer@gmail.com>
2024-03-28 08:02:43 -0700

    Modules in python

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#16 File Handling in python.py
@@ -0,0 +1,9 @@
+#16 File Handling and Modules in python
+'''def adding(a,b):
+    sum = a + b
+    print("The sum off two number is :{}".format(sum))
+def subtract(a,b):'''
+
+import pyfiglet
+banner = pyfiglet.figlet_format("Delowar.Designer")
+print(banner)
