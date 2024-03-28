96ee65b6d7074b989dae808e8ae7d02795e99881
Delowar-Designer <delowar.designer@gmail.com>
2024-03-28 07:11:25 -0700

    Data Structure list, tuple in python

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#14 Data Structure list, tuple in python.py
@@ -0,0 +1,22 @@
+#14 Data Structure list, tuple in python
+data = [1,2,3,4,5,6,7,8,9]
+value = ["1","mitu","boy","joty"]
+
+print(data)
+print(value)
+print(type(value))
+print(value[1])
+value1 = (1,2,3,4,5,6,7,8,9)
+print(value1)
+print(type(value1))
+
+thisdict = {
+    "firstname" : "Mitu",
+    "lastname" : "Shamiea",
+    "age" : 19
+}
+print(thisdict["firstname"])
+
+data2 = [1,2,3,4,5,6,7,8,9]
+for i in data:
+    print(i)
