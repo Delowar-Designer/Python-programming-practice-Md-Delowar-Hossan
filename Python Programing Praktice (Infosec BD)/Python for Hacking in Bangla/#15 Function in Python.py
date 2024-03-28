592ad32869abf6e61f7924f09f0d46b7461a4c65
Delowar-Designer <delowar.designer@gmail.com>
2024-03-28 07:37:48 -0700

    Function in Python

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#15 Function in Python.py
@@ -0,0 +1,15 @@
+#15 Function in Python
+def data():
+    print("This is me Delowar")
+data()
+
+a = 10
+b = 20
+sum = a+b
+print(sum)
+
+def da(a,b):
+    print("This is function output")
+    sum = a+b
+    print(sum)
+da(20,5)
