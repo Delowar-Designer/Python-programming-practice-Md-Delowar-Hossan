5e410813a13e405717d17e095209d9e65be64b30
Delowar-Designer <delowar.designer@gmail.com>
2024-03-29 04:43:52 -0700

    Varibles, Data_types, List, Tuple, Dicctionary

---
+++ Python Programming (Coding For Everyone)/Python Tutorial/#2 Varibles, Data_types, List, Tuple, Dicctionary.py
@@ -0,0 +1,30 @@
+#2 Varibles, Data_types, List, Tuple, Dicctionary
+num = 10
+num1 = None
+print(num1)
+print(num)
+num = "Delowar"
+print(num)
+# Datatype
+str = 'Delowar for everyone'
+print(str[2:7])
+print(str+' test')
+
+# Lists
+my_list = [1,2,3,4,5,6,7,8,9]
+list = ["span","egg","pen","apple"]
+print(my_list)
+print(my_list[0:4])
+print(my_list * 2)
+print(my_list + list)
+
+# Tuples
+my_tuple = (1,2,3,4,5,6,7,8,9,"Delowar","Mitu","Joty")
+my_tuple2 = (1414,58,2,4,5,6,7,8,9,"Delowar")
+print(my_tuple)
+print(my_tuple[:2])
+print(my_tuple*2)
+print(my_tuple + my_tuple2)
+
+# Dictionary
+
