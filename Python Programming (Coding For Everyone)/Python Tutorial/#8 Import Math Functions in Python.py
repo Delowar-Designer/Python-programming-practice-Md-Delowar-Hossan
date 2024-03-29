8cf137c06b18b3cc92e9c7b1455eb55a26ad4a89
Delowar-Designer <delowar.designer@gmail.com>
2024-03-29 07:36:36 -0700

    Import Math Functions in Python

---
+++ Python Programming (Coding For Everyone)/Python Tutorial/#8 Import Math Functions in Python.py
@@ -0,0 +1,18 @@
+#8 Import Math Functions in Python
+import math
+from math import factorial,pow,floor
+x = math.sqrt(15)
+print(x)
+
+x = math.pow(3,3)
+print(x)
+
+x = math.floor(3.6)
+print(x)
+x = math.ceil(3.1)
+print(x)
+
+x = math.factorial(4)
+print(x)
+
+help('math')
