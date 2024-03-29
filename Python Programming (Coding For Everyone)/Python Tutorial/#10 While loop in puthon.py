63ec1cead42e6f916c04364e10dd589408138016
Delowar-Designer <delowar.designer@gmail.com>
2024-03-29 08:17:18 -0700

    While loop in puthon

---
+++ Python Programming (Coding For Everyone)/Python Tutorial/#10 While loop in puthon.py
@@ -0,0 +1,16 @@
+#10 While loop in puthon
+i = 1
+while i < 10:
+    print("Programmer Delowar.Designer")
+    i = i+1
+
+i = 1
+print(i)
+while i<3:
+    print("inside i",i)
+    j = 1
+    while j<3:
+        print("inside j",j)
+        j = j + 1
+    print()
+    i = i + 1
