aacf2dbab56128a903d1d213185a13cc8b8cdd29
Delowar-Designer <delowar.designer@gmail.com>
2024-03-29 20:00:23 -0700

    Functions in python

---
+++ Python Programming (Coding For Everyone)/Python Tutorial/#14 Functions in python.py
@@ -0,0 +1,14 @@
+# Functions in python
+def hello():
+    print("Hello Programer")
+hello()
+
+def add(x,y):
+    z = x+y
+    a = x-y
+    return z,a
+    '''print("Value of z is ",z)
+add(2,3)'''
+sum,sub=add(3,5)
+print(sum)
+print(sub)
