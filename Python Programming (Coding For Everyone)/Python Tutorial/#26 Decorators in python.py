fe756240e4d8fe5cfeb5f867e755b6a648cbe6c8
Delowar-Designer <delowar.designer@gmail.com>
2024-03-30 19:14:13 -0700

    Decorators in python

--- Python Programming (Coding For Everyone)/Python Tutorial/#26 Decorators in python.py
+++ Python Programming (Coding For Everyone)/Python Tutorial/#26 Decorators in python.py
@@ -5,3 +5,27 @@ def test1():
 test2=test1
 del test1
 test2()
+def test3(num):
+    if num==0:
+        return print
+    if num ==1:
+        return sum
+ret=test3(0)
+print(ret)
+
+def test4(func):
+    func("in test function")
+test4(print)
+
+def test5(func):
+    def exec():
+        print("first line of exec")
+        func()
+        print("3rd line of exec")
+    return exec
+@test5
+def my_func():
+    print("Please to my chake")
+
+#my_func = test5(my_func)
+my_func()
