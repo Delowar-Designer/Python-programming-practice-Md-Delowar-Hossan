18999291d87dbafa8267f1ae9cdd8cee6f8ba19d
Delowar-Designer <delowar.designer@gmail.com>
2024-03-30 19:43:25 -0700

    name variable in python

---
+++ Python Programming (Coding For Everyone)/Python Tutorial/#28 name variable in python.py
@@ -0,0 +1,6 @@
+#28 name variable in python
+import demo
+print(__name__)
+
+print("Hello World")
+print("Hello user form demo")
---
+++ Python Programming (Coding For Everyone)/Python Tutorial/demo.py
@@ -0,0 +1,8 @@
+#print(__name__)
+def grreet():
+    print("Hello Delowar")
+    print("Hello Mitu Do not takte")
+if __name__ == "__main__":
+    grreet()
+else:
+    print("I am form Demo")
