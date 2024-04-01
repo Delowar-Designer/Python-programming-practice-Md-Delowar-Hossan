71de8e56ac8f48442428f5e2248b473c7ae048c8
Delowar-Designer <delowar.designer@gmail.com>
2024-04-01 04:14:58 -0700

    Creating and Deleting Table

---
+++ Python Programming (Coding For Everyone)/MySQL/Creating and Deleting Table.sql
@@ -0,0 +1,13 @@
+create database Student;
+show databases;
+
+use Student;
+create table STUUDENT_DETAILS(
+ID INT,
+NAME varchar(255),
+ADDRESS varchar(255),
+ROLL varchar(255));
+
+SELECT * from STUUDENT_DETAILS;
+
+DROP TABLE STUUDENT_DETAILS;
