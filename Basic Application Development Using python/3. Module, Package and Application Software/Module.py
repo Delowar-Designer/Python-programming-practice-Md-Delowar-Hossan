f054e026c5f725401291198074b86f9ea90fcba4
Delowar-Designer <delowar.designer@gmail.com>
2024-04-10 05:18:05 -0700

    Module Testmodule

---
+++ Basic Application Development Using python/3. Module, Package and Application Software/Module.py
@@ -0,0 +1,3 @@
+import testmodule
+print("Addition....:", testmodule.add_fun(7,6))
+print("Subtraction....:", testmodule.add_fun(2,3))
---
+++ Basic Application Development Using python/3. Module, Package and Application Software/testmodule.py
@@ -0,0 +1,10 @@
+# Module and package
+# Function Definition for addition
+def add_fun(a,b):
+    c = a + b
+    return c
+
+# Function Deffinition for addition
+def sub_fun(a,b):
+    c = a - b
+    return c
