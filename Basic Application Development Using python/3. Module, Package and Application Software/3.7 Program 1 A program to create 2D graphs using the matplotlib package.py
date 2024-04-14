6fd18f5a1eff2736553c0dfa212477929f681ae9
Delowar-Designer <delowar.designer@gmail.com>
2024-04-14 03:23:50 -0700

    A program to create 2D graphs using the matplotlib package

---
+++ Basic Application Development Using python/3. Module, Package and Application Software/3.7 Program 1 A program to create 2D graphs using the matplotlib package.py
@@ -0,0 +1,12 @@
+# A program to create 2D graphs using the matplotlib package
+# importing matplottlib module
+from matplotlib import pyplot as plt
+
+# x-axis values
+x = [5,2,9,4,7]
+# y-axis values
+y = [10,5,8,4,2]
+# Function to plot
+plt.plot(x,y)
+# Function to show the plot
+plt.show()
