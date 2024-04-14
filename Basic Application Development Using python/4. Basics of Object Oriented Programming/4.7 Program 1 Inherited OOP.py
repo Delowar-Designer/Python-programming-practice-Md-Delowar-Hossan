ca61694dfbffdf1db7bee7f9828511d4ad1f25ff
Delowar-Designer <delowar.designer@gmail.com>
2024-04-14 08:46:45 -0700

    Inherited OOP

---
+++ Basic Application Development Using python/4. Basics of Object Oriented Programming/4.7 Program 1 Inherited OOP.py
@@ -0,0 +1,14 @@
+# Inherited OOP
+class Animal:
+    def __init__(self,name):
+        self.name = name
+        print(self.name + "was adopted.")
+    def run(self):
+        print("running!")
+class Dog(Animal):
+    def bark(self):
+        print("woof!")
+# new dog behavior inherited from Animal parent class
+spot = Dog("spot") #=> spot was adopted.
+spot.run() # => running!
+
