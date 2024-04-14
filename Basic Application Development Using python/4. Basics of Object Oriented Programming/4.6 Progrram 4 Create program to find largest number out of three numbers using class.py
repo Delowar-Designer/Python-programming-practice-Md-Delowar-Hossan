47c29077011ace94c44f4bc9332cb3ee0fbb7d27
Delowar-Designer <delowar.designer@gmail.com>
2024-04-14 08:17:56 -0700

    Create program to find largest number out of three numbers using class

---
+++ Basic Application Development Using python/4. Basics of Object Oriented Programming/4.6 Progrram 4 Create program to find largest number out of three numbers using class.py
@@ -0,0 +1,13 @@
+# Create program to find largest number out of three numbers using class
+class max3num:
+    def __init__(self, a, b, c):
+        if(a>b)and(a>c):
+            print("A is maximum: ",a)
+        elif(b>c):
+            print("B is maximum: ",b)
+        else:
+            print("C is maximum: ",c)
+a = int(input("A = "))
+b = int(input("B = "))
+c = int(input("C = "))
+max3num = max3num(a,b,c)
