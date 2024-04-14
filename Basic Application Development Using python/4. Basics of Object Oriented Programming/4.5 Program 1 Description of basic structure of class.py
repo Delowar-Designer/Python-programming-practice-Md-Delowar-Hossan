5755c565cf736d018bfb0d5f7d7169c3277fce39
Delowar-Designer <delowar.designer@gmail.com>
2024-04-14 07:45:21 -0700

    Description of basic structure of class

---
+++ Basic Application Development Using python/4. Basics of Object Oriented Programming/4.5 Program 1 Description of basic structure of class.py
@@ -0,0 +1,9 @@
+# Description of basic structure of class
+class Car:
+    name = "Mitu"
+    color = "Red"
+    def start():
+        print("Starting the engine")
+print("Name of the car:", Car.name)
+print("Color:", Car.color)
+Car.start()
