8b358091bc4fbeafd658924a07d12e0675df4a22
Delowar-Designer <delowar.designer@gmail.com>
2024-04-14 10:25:28 -0700

    Encapsulation OOP

---
+++ Basic Application Development Using python/5. Four Pillars of OOP/5.1 Program 4 Encapsulation OOP.py
@@ -0,0 +1,19 @@
+# Encapsulation OOP
+class Fish:
+    def __init__(self):
+        self.__size = "big"
+    def get_size(self):
+        print("I am " + self.__size + " fish")
+    def set_size(self, new_size):
+        self.__size = new_size
+# using the getter method
+oscar = Fish()
+oscar.get_size() # => I am big fish
+# change the size
+bert = Fish()
+bert.__size = "small"
+bert.get_size() # => I am big fish
+# using setter method
+fin = Fish()
+fin.set_size("tiny")
+fin.get_size() #=> I am a tiny fish
