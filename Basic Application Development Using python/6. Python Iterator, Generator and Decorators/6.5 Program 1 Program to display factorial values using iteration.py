34be037bc7e662fa2d2306919fdfed7bb84efad5
Delowar-Designer <delowar.designer@gmail.com>
2024-04-15 05:21:39 -0700

    Program to display factorial values using iteration

---
+++ Basic Application Development Using python/6. Python Iterator, Generator and Decorators/6.5 Program 1 Program to display factorial values using iteration.py
@@ -0,0 +1,42 @@
+# Program to display factorial values using iteration
+# Programm of custom iterator which prints the factorial of numberrs till the limit
+# numbers is reached.
+class factorila:
+    # constructor
+    # intializing the limit
+    def __init__(self, limit):
+        self.limit = limit
+
+    # returns an iterator object when called.
+    def __iter__(self):
+        self.num = 1
+        self.fact = 1
+        return self
+    # returns the factorial of next number
+    # until the numberr <= limit.
+    def __next__(self):
+
+        # current number.
+        curr_num = self.num
+
+        # current number factorial
+        curr_fact = self.fact
+
+        # if current number <= limit
+        if curr_num <= self.limit:
+            # updating number for next iteration
+            self.num = self.num + 1
+
+            # Updating factorial for next iteration
+            self.fact = self.fact * self.num
+
+            # returning curent number factorial
+            return curr_fact
+        # if number exceeds limit raise error
+        else:
+            raise StopIteration
+# intialzing the object of the class factorial
+fact = factorila(5)
+for x in fact:
+    print(x)
+
