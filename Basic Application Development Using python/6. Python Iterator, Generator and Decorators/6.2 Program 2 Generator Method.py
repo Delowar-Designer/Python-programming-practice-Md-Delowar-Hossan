ed89278347340809c8b2483bd724c715d0adb8ff
Delowar-Designer <delowar.designer@gmail.com>
2024-04-14 18:56:41 -0700

    Generator Method

---
+++ Basic Application Development Using python/6. Python Iterator, Generator and Decorators/6.2 Program 2 Generator Method.py
@@ -0,0 +1,10 @@
+# Generator Method
+def sq_numbers(n):
+    for i in range(1, n+1):
+        yield i*i
+a = sq_numbers(4)
+print("The square of numbers 1,2,3 are: ")
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
