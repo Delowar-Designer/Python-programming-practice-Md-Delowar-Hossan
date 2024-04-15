db3b3bf7d2534dc0213b15dd399a25e1fa0903f2
Delowar-Designer <delowar.designer@gmail.com>
2024-04-15 09:19:34 -0700

    Program using tri export and hilsa by creating userD fine function

---
+++ Basic Application Development Using python/7. Exception & Error Handling in Python/7.6 Program 5 Program using Asset Tree Export by creating userdefined functions.py
@@ -0,0 +1,8 @@
+# Program using Asset Tree Export by creating userdefined functions
+def square(x):
+    assert x>=0, 'Only positive numbers are allowed'
+    return x*x
+try:
+    square(-2)
+except AssertionError as msg:
+    print(msg)
