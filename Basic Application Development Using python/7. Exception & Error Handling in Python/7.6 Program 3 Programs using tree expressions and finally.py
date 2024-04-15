c646e46f0db267ce271e320d9ce7fe54e87cccf1
Delowar-Designer <delowar.designer@gmail.com>
2024-04-15 09:05:41 -0700

    Programs using tree expressions and finally

---
+++ Basic Application Development Using python/7. Exception & Error Handling in Python/7.6 Program 3 Programs using tree expressions and finally.py
@@ -0,0 +1,9 @@
+# Programs using tree expressions and finally
+try:
+    x>3
+except:
+    print("Something went wrong")
+else:
+    print("Nothing went wrong")
+finally:
+    print("The try except block is finished")
