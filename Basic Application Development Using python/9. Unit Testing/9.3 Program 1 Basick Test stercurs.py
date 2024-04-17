af1a73f75d074a6b99c9a647847e22fc63fa87b8
Delowar-Designer <delowar.designer@gmail.com>
2024-04-17 03:39:46 -0700

    unittest

---
+++ Basic Application Development Using python/9. Unit Testing/9.3 Program 1 Basick Test stercurs.py
@@ -0,0 +1,9 @@
+# Basick Test stercurs
+import unittest
+class SimpleTest(unittest.TestCase):
+    # Returns True or False.
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
