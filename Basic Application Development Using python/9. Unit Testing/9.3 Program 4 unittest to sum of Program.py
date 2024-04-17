cb0fe78c17d297b49881d14f3a3cb8aaa8e594d9
Delowar-Designer <delowar.designer@gmail.com>
2024-04-17 04:53:13 -0700

    unittest to sum of Program

---
+++ Basic Application Development Using python/9. Unit Testing/9.3 Program 4 unittest to sum of Program.py
@@ -0,0 +1,12 @@
+# unittest to sum of Program
+import unittest
+from my_unittest import add, check_even
+class MyTest(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(4,3),7)
+
+    def test_check_even(self):
+        self.assertTrue(check_even(4))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Basic Application Development Using python/9. Unit Testing/my_unittest.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    return x+y
+
+def check_even(number):
+    if (number % 2) == 0:
+        return True
+    else:
+        return False
+    
