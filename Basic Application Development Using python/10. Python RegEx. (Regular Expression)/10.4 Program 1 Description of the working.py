68a6647abf5f11e3136fcc683de5f72d981e488d
Delowar-Designer <delowar.designer@gmail.com>
2024-04-18 21:21:39 -0700

    Description of the working Substituting

---
+++ Basic Application Development Using python/10. Python RegEx. (Regular Expression)/10.4 Program 1 Description of the working.py
@@ -0,0 +1,9 @@
+# Description of the working Substituting
+import re
+def substitutor():
+    sentence1 = "It is raining outside."
+    print(re.sub(r"raining","sunny", sentence1))
+    sentence2 = "Thank you very very much."
+    print(re.sub(r"very","so",sentence2))
+substitutor()
+
