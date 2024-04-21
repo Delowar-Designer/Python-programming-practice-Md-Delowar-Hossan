644a791a8197829b74a93bbb64d01d07a8dd8386
Delowar-Designer <delowar.designer@gmail.com>
2024-04-20 21:25:23 -0700

    User input

---
+++ Python Programing (Black0sRa)/Python Programing/#2 User input.py
@@ -0,0 +1,8 @@
+# User input
+print("Hi Mitu", 'Congratulations')
+print("what is your name: ")
+name = input()
+print("Hello name is "+name)
+print(f"Hi {name} Congratulations")
+age = input("wat is your age : ")
+print(f"my age is {age} year olde")
