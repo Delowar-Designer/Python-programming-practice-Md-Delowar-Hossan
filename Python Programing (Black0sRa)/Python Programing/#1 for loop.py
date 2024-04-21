df48f52e5858926bdc0f01b315a8e20e3811299c
Delowar-Designer <delowar.designer@gmail.com>
2024-04-20 21:17:36 -0700

    Data Types

---
+++ Python Programing (Black0sRa)/Python Programing/#1 for loop.py
@@ -0,0 +1,35 @@
+# for loop
+'''for(int i =0;i<5;i++){
+    System.out.println(i);
+}
+
+for i in range(0,5):
+    print(i)
+'''
+# commint
+# celsius
+'''temperature = 36 # ctrl + /
+
+# Fahrenheit
+temperature = 93
+print(temperature)
+'''
+# variable
+print("I am student")
+print('I am "a" student')
+print(5+8)
+print((3+2)+5)
+number = 3+4
+print(number)
+new_number = number +5
+print(new_number)
+# Data Types
+name = 'Mitu'
+age = 21
+print(name)
+print(age)
+print(str(21) + name)
+print(type(name))
+print(type(age))
+ratio = 2.3
+print(type(ratio))
