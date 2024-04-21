9b3b09cf897122c75cd0b50182117a3d6fafe629
Delowar-Designer <delowar.designer@gmail.com>
2024-04-20 21:50:19 -0700

    Numbers and Math

---
+++ Python Programing (Black0sRa)/Python Programing/#3 Numbers and Math.py
@@ -0,0 +1,56 @@
+# Numbers and Math
+add = 2+ 4
+sub = 5-3
+mul = 5*5
+print(add)
+print(sub)
+print(mul)
+'''
+print("What is your name: ")
+name = input()
+print(name,"How old are your?")
+age = int(input())
+
+deays = age * 365
+minutes = age * 525948
+seconds = age * 31556926
+
+print(name,'you has lived for',deays,'days',minutes,'minutes',seconds,'seconds')'''
+
+
+
+div = 10/2
+print(div)
+div = 7//2
+print(div)
+reminder =7%2
+print(reminder)
+
+a = 5
+b = 2
+print("Max",max(a,b))
+print("Min",min(a,b))
+
+numbers = 2 ** 5
+print(numbers)
+
+x = 5
+y = x + 5
+print(y)
+print(x)
+x -= 3
+print(x)
+
+number = 10 + 3 - 4 * 5 ** 4
+print(number)
+print(abs(number))
+number = 7/2
+print(round(number))
+
+import math
+
+value = 16
+result = math.sqrt(value)
+print(result)
+print(math.ceil(4.1))
+print(math.floor(4.1))
