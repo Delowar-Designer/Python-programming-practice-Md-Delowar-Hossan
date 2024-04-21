4e6e3b733e0b6b811bcc50efe14c6b1e3e97cfcb
Delowar-Designer <delowar.designer@gmail.com>
2024-04-20 22:06:44 -0700

    String in Python

---
+++ Python Programing (Black0sRa)/Python Programing/#4 String in Python.py
@@ -0,0 +1,16 @@
+# String in Python
+course_name = "We are learning Python Programming"
+print(len(course_name))
+new_line = len(course_name)
+print(new_line)
+print(course_name.upper())
+print(course_name.title())
+print(course_name.find('a'))
+print(course_name[1])
+print(course_name.replace('are', 'is'))
+print(course_name.replace('e', 'E'))
+print(course_name.endswith('g'))
+slice_name = course_name[:-4]
+print(slice_name)
+print(course_name[-4])
+
