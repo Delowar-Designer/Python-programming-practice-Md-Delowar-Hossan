1087ba3f5fa7e3345b9da8af51c64f23dcb97a5b
Delowar-Designer <delowar.designer@gmail.com>
2024-04-21 01:26:58 -0700

    Guessing Game

---
+++ Python Programing (Black0sRa)/Python Programing/#10 Guessing Game.py
@@ -0,0 +1,12 @@
+# Guessing Game
+secret_number = 5
+guess_count = 1
+guess_limit = 3
+while guess_count <= guess_limit :
+    user_guess_number = int(input("Guess > "))
+    guess_count += 1
+    if user_guess_number == secret_number:
+        print("You win !!")
+        break
+else:
+    print("You Loss .. :( ")
