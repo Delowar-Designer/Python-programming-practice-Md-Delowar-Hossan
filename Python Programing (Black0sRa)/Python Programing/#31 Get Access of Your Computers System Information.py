03a9ce4ae1f37b38bf2cb8feaf5c2bfb0b09c7f4
Delowar-Designer <delowar.designer@gmail.com>
2024-04-26 04:09:51 -0700

    Get Access of Your Computers System Information

---
+++ Python Programing (Black0sRa)/Python Programing/#31 Get Access of Your Computers System Information.py
@@ -0,0 +1,15 @@
+# Get Access of Your Computers System Information
+import platform
+
+print(platform.system())
+print(platform.version())
+print(platform.platform())
+print(platform.machine())
+print(platform.processor())
+print(platform.architecture())
+print(platform.python_version())
+print(platform.node())
+print(platform.uname())
+print(platform.release())
+print(platform.python_branch())
+print(platform.java_ver())
