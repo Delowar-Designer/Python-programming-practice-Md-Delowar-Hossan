3274f96e014ac9dfdf89c0dd939e736bcccf6305
Delowar-Designer <delowar.designer@gmail.com>
2024-04-26 03:55:27 -0700

    Python With programming Language

---
+++ Python Programing (Black0sRa)/Python Programing/#30 Python With programming Language.py
@@ -0,0 +1,139 @@
+# Python With programming Language
+import turtle
+wn = turtle.Screen()
+wn = turtle.bgcolor('light green')
+wn = turtle.title('Saon Srabon')
+sk = turtle.Turtle()
+sk.pensize(10)
+
+# this is for making some seconds so that i can maximixe the turtle window
+sk.penup()
+for i in range(1):
+    sk.circle(100)
+sk.setx(-500)   # for stting the starting point at the left top
+sk.sety(100)
+sk.pendown()
+
+# Drawing B
+sk.right(90)
+sk.forward(200)
+sk.right(-90)
+sk.forward(40)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(70)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(40)
+sk.right(180)
+sk.forward(40)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(50)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(40) # end of B
+
+# Blank Line
+sk.penup()
+sk.right(180)
+sk.forward(105)
+sk.pendown()
+# Blank Line
+
+# Drawing I
+sk.forward(100)
+sk.left(180)
+sk.forward(50)
+sk.left(90)
+sk.forward(200)
+sk.left(-90)
+sk.forward(50)
+sk.left(180)
+sk.forward(100)
+
+# Blank Linne
+sk.penup()
+sk.forward(55)
+sk.pendown()
+# Blank Line
+
+# Drowing N
+sk.left(90)
+sk.forward(200)
+sk.right(150)
+sk.forward(230)
+sk.left(150)
+sk.forward(200)
+
+# Blank Line
+sk.penup()
+sk.right(90)
+sk.forward(80)
+sk.pendown()
+# Blank line
+
+# Drowing 0
+sk.forward(80)
+sk.right(45)
+sk.forward(30)
+sk.right(45)
+sk.forward(160)
+sk.left(-45)
+sk.forward(30)
+sk.left(-45)
+sk.forward(80)
+sk.right(45)
+sk.forward(30)
+sk.right(45)
+sk.forward(160)
+sk.right(45)
+sk.forward(30)
+
+# Blank Line
+sk.penup()
+sk.right(45)
+sk.forward(155)
+sk.pendown()
+# Blank Line
+
+# Drawing D
+sk.right(90)
+sk.forward(200)
+sk.right(-90)
+sk.forward(35)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(160)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(35)
+
+sk.penup()
+sk.right(180)
+sk.forward(115)
+sk.pendown()
+
+sk.right(90)
+sk.forward(200)
+sk.right(-90)
+sk.forward(35)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(160)
+sk.left(45)
+sk.forward(30)
+sk.left(45)
+sk.forward(35)
+
+turtle.done()
+# fnished--------
+
+
