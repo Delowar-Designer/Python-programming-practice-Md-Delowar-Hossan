8d1c36dd15e863051da5bd2928eefea8ecca3b95
Delowar-Designer <delowar.designer@gmail.com>
2024-04-27 01:53:00 -0700

    Tuple Tutorial in Python

---
+++ Python Programing (CoderHobo)/Pytthon Basic To Advance/13 List in Python.py
@@ -0,0 +1,26 @@
+# List in Python
+my_list = [1,2,3,"Mitu",4,5,6]
+print(my_list[3])
+print(my_list[-2])
+print(type(my_list))
+print(len(my_list))
+
+# Listt Slicig in Python
+my_list1 = [1,2,3,4,5,6,7,8]
+print(my_list1[2:4])
+print(my_list1[1:6:2])
+print(my_list1[-4:-2])
+
+# Tuple Tutorial in Python
+my_list2 = (1,2,5.6,'Mitu','Jotty','Yafta')
+print(len(my_list2))
+print(type(my_list2))
+print(my_list2[4])
+
+my_list3 = (1,2,3,4,5,6,7,8)
+print(my_list3[2:5:1])
+print(my_list3[-4:-1])
+
+
+
+
