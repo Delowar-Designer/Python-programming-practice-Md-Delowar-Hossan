6c7f5e13b52dea1c9b8d453bd4ed9cd0c0ffa52f
Delowar-Designer <delowar.designer@gmail.com>
2024-04-27 01:31:31 -0700

    Operator in Python

---
+++ Python Programing (CoderHobo)/Pytthon Basic To Advance/12 Operator in Python.py
@@ -0,0 +1,45 @@
+# Operator in Python
+a = 4
+b = 3
+sum  = a + b
+print(sum)
+minus = a - b
+print(minus)
+mul = b*a
+print(mul)
+div = b/a
+print(div)
+div1 = b//a
+print(div1)
+modulus = b % a
+print(modulus)
+power = b ** a
+print(power)
+
+a += 3
+a = a+3
+b -= 1
+print(b)
+c = 2
+c *= 2
+print(c)
+
+
+x = 5 == 5
+print(x)
+x = 5 != 5
+print(x)
+x = 5 > 6
+print(x)
+x = 5 < 6
+print(x)
+x = 5 <=5
+print(x)
+x = 5 >= 5
+print(x)
+x = 8 < 6 and 6 < 7
+print(x)
+x = 8 < 6 or 6 < 7
+print(x)
+x = not(8 < 6 or 6 < 7)
+print(x)
