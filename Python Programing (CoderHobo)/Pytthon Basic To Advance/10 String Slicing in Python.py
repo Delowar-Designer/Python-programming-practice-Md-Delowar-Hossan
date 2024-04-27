1aaf2eb1e598fcdfa867d7471a7b120ff6749e4a
Delowar-Designer <delowar.designer@gmail.com>
2024-04-27 01:16:38 -0700

    String Modily Python

---
+++ Python Programing (CoderHobo)/Pytthon Basic To Advance/10 String Slicing in Python.py
@@ -0,0 +1,28 @@
+# 10 String Slicing in Python
+n  =  "This is String"
+print(n[5:7])
+print(n[:5])
+print(n[-6:-1])
+
+# String Modily Python
+name = "Tutrialhubs"
+print(name.upper())
+a = "PROGRAMER DELOWAR.DESIGNER"
+print(a.lower())
+b = "    Mitu  "
+print(b.strip())
+print(b.lstrip())
+print(b.rstrip())
+
+c = "Joty"
+print(c.replace("J","M"))
+
+d = "Hello World"
+print(d.split(" "))
+e = d.split(" ")
+print(e[0])
+
+f = "Simay"
+g = "Mitu"
+h = f + " "+ g
+print(h)
