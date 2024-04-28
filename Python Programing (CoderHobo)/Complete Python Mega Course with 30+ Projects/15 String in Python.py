48b28c1a15e18dc73f251e093efe2761b02ad766
Delowar-Designer <delowar.designer@gmail.com>
2024-04-28 08:43:07 -0700

    String in Python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/15 String in Python.py
@@ -0,0 +1,10 @@
+# String in Python
+name = "Delowar"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.replace("D","M"))
+name2 = "   Mitu  "
+print(name2.strip())
+name3 = "delowar@gmsil.com"
+print(name3.split("."))
