4f9ab1a7cdd727a20ab806c53a765e118b881f1a
Delowar-Designer <delowar.designer@gmail.com>
2024-04-28 05:24:28 -0700

    Function with parameter in Python

---
+++ Python Programing (CoderHobo)/Pytthon Basic To Advance/23 Function with parameter in Python.py
@@ -0,0 +1,31 @@
+# Function with parameter in Python
+def my_function(name,roll):
+    print("Hi",name)
+    print("roll is",roll)
+
+my_function(name= "Mitu",roll=5825)
+
+def my_fank(age = 25):
+    print("Mitu",age)
+
+my_fank("Delowar")
+my_fank()
+
+
+def my_func(my_li):
+    print(my_li)
+
+my_list = {1,2,3,4,5,6}
+my_func(my_list)
+
+
+def my_func(n):
+    sum =  20 + n
+    return sum
+
+n = my_func(5)
+print(n)
+
+def my_func(n):
+    pass
+print("Mitu")
