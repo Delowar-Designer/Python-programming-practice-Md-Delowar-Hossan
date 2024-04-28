7e4dae1f87d21eb4aca58a07fef56b1b2b69d3f4
Delowar-Designer <delowar.designer@gmail.com>
2024-04-28 05:34:26 -0700

    Nasteat Condition in python

--- Python Programing (CoderHobo)/Pytthon Basic To Advance/31 Nasteat Condition in python.py
+++ Python Programing (CoderHobo)/Pytthon Basic To Advance/31 Nasteat Condition in python.py
@@ -1,1 +1,9 @@
 # Nasteat Condition in python
+a = 60
+if (a > 10):
+    if (a < 50):
+        print("a is less than 50")
+    else:
+        print("a is not less than 50")
+else:
+    print("not greater than 10")
