998357c137023d5905e820510440aa182602d6ce
Delowar-Designer <delowar.designer@gmail.com>
2024-04-28 23:20:32 -0700

    Data Type in Python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/16 Data Type in Python.py
@@ -0,0 +1,22 @@
+# Data Type in Python
+print(10 > 6)
+print(10 < 6)
+print(10 == 10)
+
+# Operator in Python
+a = 20
+b = 3
+sum = a + b
+print(sum)
+sub = a - b
+print(sub)
+mul = a * b
+print(mul)
+div = a / b
+print(div)
+div1 = a // b
+print(div1)
+expo = a ** b
+print(expo)
+modulus = a % b
+print(modulus)
