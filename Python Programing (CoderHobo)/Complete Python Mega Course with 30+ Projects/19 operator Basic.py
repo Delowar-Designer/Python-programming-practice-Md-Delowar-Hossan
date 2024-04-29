efb848c3b840a81ee1ba33920f0e40f5d7b9035b
Delowar-Designer <delowar.designer@gmail.com>
2024-04-29 01:43:50 -0700

    operator Basic

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/19 operator Basic.py
@@ -0,0 +1,11 @@
+# operator Basic
+print(10 == 10)
+print(10 != 5)
+print(10 > 5)
+print(10 < 5)
+print(190 >= 10)
+print(5 <= 10)
+print(10 > 5 and 5 < 2)
+print(10 > 5 or 5 < 2)
+print(not(10 <20))
+
