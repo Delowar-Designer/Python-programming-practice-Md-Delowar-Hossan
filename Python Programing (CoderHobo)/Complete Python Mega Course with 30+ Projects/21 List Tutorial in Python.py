f8a6bf36173fa803f31fb92c4814218c7a521352
Delowar-Designer <delowar.designer@gmail.com>
2024-04-29 02:09:46 -0700

    List Tutorial in Python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/21 List Tutorial in Python.py
@@ -0,0 +1,27 @@
+#List Tutorial in Python
+name = ['Mitu', 'Joty',"Yafta"]
+print(name)
+print(type(name))
+nam = [2,3,4,5,6]
+num2 = [1.2,2.3,4.6,6.7]
+my_bool = [True, False]
+print(nam)
+print(num2)
+print(my_bool)
+print(name[-1])
+print(name[0])
+
+my_list = [1,2,3,4,5,'Mohot','coderhobo']
+print(my_list)
+my_list.append(True)
+print(my_list)
+my_list.insert(3,34)
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.pop(2)
+print(my_list)
+my_list.remove(2)
+print(my_list)
+my_list[0] = 10
+print(my_list)
