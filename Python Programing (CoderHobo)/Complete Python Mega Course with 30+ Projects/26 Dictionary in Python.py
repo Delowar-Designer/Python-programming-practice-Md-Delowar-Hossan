2e56d44ba21be722cb5fee989bdc62ac9bbb8550
Delowar-Designer <delowar.designer@gmail.com>
2024-05-09 09:55:33 -0700

    Dictionary in Python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/26 Dictionary in Python.py
@@ -0,0 +1,14 @@
+# Dictionary in Python
+my_dict = {
+    "name" : "Mitu",
+    "Roll" : 602826,
+    'age' : 24,
+    'age' : 5878
+}
+print(my_dict)
+print(type(my_dict))
+print(my_dict['age'])
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+print(len(my_dict))
