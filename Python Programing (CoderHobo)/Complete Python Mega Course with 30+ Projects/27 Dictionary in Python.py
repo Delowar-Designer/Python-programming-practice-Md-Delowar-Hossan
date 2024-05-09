16ee517f1b01bf698899f611238fd0b8d955dfd3
Delowar-Designer <delowar.designer@gmail.com>
2024-05-09 10:04:41 -0700

    Dictionary in Python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/27 Dictionary in Python.py
@@ -0,0 +1,13 @@
+# Dictionary in Python
+my_dict = {
+    "name" : "Delowar",
+    'age' : 19
+}
+my_dict["age"] = 99
+print(my_dict)
+my_dict['colo'] = 'red'
+print(my_dict)
+my_dict.pop('age')
+print(my_dict)
+my_dict.popitem()
+print(my_dict)
