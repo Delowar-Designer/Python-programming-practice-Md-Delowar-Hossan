64d03caa1628d72b4fccfbe2eae1db9cb60b63e7
Delowar-Designer <delowar.designer@gmail.com>
2024-05-09 09:47:39 -0700

    Set in Python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/25 Set in Python.py
@@ -0,0 +1,10 @@
+# Set in Python
+my_sets = {'Mitu', 'Joty','yafta',1,0,2,3,4,5,6,7,True, False}
+print(my_sets)
+print(type(my_sets))
+my_sets.add(45)
+print(my_sets)
+my_sets.remove('yafta')
+print(my_sets)
+my_sets.discard(2)
+print(my_sets)
