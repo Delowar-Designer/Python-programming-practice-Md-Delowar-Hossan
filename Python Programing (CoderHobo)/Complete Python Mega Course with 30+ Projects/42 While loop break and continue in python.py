ae775af000200334a47bdfee4bc833acc0f2379a
Delowar-Designer <delowar.designer@gmail.com>
2024-05-11 05:34:10 -0700

    While loop break and continue in python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/42 While loop break and continue in python.py
@@ -0,0 +1,15 @@
+# While loop break and continue in python
+'''i = 1
+while (i < 10):
+    if(i == 3):
+        break
+    print(i)
+    i+=1'''
+
+
+i = 1
+while (i < 10):
+    i +=1
+    if(i == 3):
+        continue
+    print(i)
