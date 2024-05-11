9ada179c4abb320169db93d87f287a54758dcbf3
Delowar-Designer <delowar.designer@gmail.com>
2024-05-11 04:43:04 -0700

    Nested  Condition in Python

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/30 Nested  Condition in Python.py
@@ -0,0 +1,11 @@
+# Nested  Condition in Python
+icecream = 25
+if(icecream < 40):
+    if(icecream == 35):
+        print("I will buy cone iceream")
+    elif(icecream == 25):
+        print("I will buy chocobar")
+    else:
+        print("I will Return Home")
+else:
+    print('Return Home!')
