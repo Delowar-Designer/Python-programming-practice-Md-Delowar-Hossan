858a3310794c985b5c66458dad681ab8d3878dcf
Delowar-Designer <delowar.designer@gmail.com>
2024-05-11 05:19:21 -0700

    Check number is even of odd in puthon

---
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/39 Check number is even of odd in puthon.py
@@ -0,0 +1,12 @@
+# Check number is even of odd in puthon
+n = int(input('Ennter a number: '))
+if(n % 2 == 0):
+    print("This number is even")
+else:
+    print("This number is odd")
+
+i = 1
+while(i <= 20):
+    if (i % 2 == 0):
+        print(i)
+    i+=1
