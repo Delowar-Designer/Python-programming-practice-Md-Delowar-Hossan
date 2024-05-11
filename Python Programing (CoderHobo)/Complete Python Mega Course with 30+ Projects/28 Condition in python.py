ecce880229610e73c177761dae20f5afb4251298
Delowar-Designer <delowar.designer@gmail.com>
2024-05-11 04:32:02 -0700

    Dictionary in python

--- Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/28 Condition in python.py
+++ Python Programing (CoderHobo)/Complete Python Mega Course with 30+ Projects/28 Condition in python.py
@@ -1,7 +1,13 @@
 # Condition in python
 icecream = 59
+chocolate = 55
+biscit = 30
 if(icecream < 55):
     print("I will buy Icecream!")
 #print('I will buy Icecream in second line')
+elif(chocolate < 55):
+    print("I will buy chocolate!")
+elif(biscit < 40):
+    print('I will buy biscit')
 else:
     print("Return Home!")
